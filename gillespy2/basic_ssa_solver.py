f9cbb4f6cbfa5ddbb83bedeb1c803a82fcc8d655
Dalton <Thisisnotdalton@users.noreply.github.com>
2017-09-29 16:46:56 -0400

    Removed old, extraneous commented codes.

--- gillespy2/basic_ssa_solver.py
+++ gillespy2/basic_ssa_solver.py
@@ -9,9 +9,6 @@ class BasicSSASolver(gillespy2.GillesPySolver):
     @classmethod
     def run(self, model, t=20, number_of_trajectories=1,
             increment=0.05, seed=None, debug=False, show_labels=False,stochkit_home=None):
-        """ TODO
-        """
-       # self = BasicSSASolver()
         self.simulation_data = []
  
   	curr_state = {} 
@@ -19,9 +16,6 @@ class BasicSSASolver(gillespy2.GillesPySolver):
 	results = {}	
 	
         for traj_num in range(number_of_trajectories):
-            # put SSA loop here
-	    
-	      
             for s in model.listOfSpecies:   #Initialize Species population
                 curr_state[s] = model.listOfSpecies[s].initial_value	
 		results[s]=[]
@@ -42,25 +36,14 @@ class BasicSSASolver(gillespy2.GillesPySolver):
 		reaction_num = None
             	for r in model.listOfReactions: 
 			propensity[r] = eval(model.listOfReactions[r].propensity_function, curr_state)
-			prop_sum += propensity[r]
-			#print('------------------------------------------propensity-------------------------') 
-			#print('time',curr_time,'r',r,propensity[r], model.listOfReactions[r].propensity_function)	
+			prop_sum += propensity[r]	
 		reaction_num = random.uniform(0,prop_sum)
-		#print('Reaction Rand: ',reaction_num)	
-		#print('Propensity Sum: ',prop_sum)
-		#for r in model.listOfReactions:
-		#	print(r,propensity[r])
 		for r in model.listOfReactions:
 			cumil_sum += propensity[r]
 	
 			if(cumil_sum >= reaction_num):
 				reaction = r
-				#print('Cumilative Sum:  ',cumil_sum)	
 				break
-
-		#print('Reaction: ',reaction)
-		#print('-'*80)
-	
 		if(prop_sum <= 0):
 			while(save_time <= t):
 				results['time'].append(save_time)
@@ -86,17 +69,6 @@ class BasicSSASolver(gillespy2.GillesPySolver):
 
 
 	    return results
-		
-		#propensity.append(eval(model.listOfReactions[r].propensity_function(), curr_state))
-           
-		 #self.simulation_data[traj_num] = {}
-            
-            #to update  propensity function i   
-          #  eval(model.listOfReactions[i].propensity_function,{},{'A':1,'B':10})
-            # append
-         #   for species_name in model.listOfSpecies:
- #               self.simulation_data[traj_num][species_name] = #time series data here
-
     def get_trajectories(self, outdir, debug=False, show_labels=False):
         if show_labels:
             return self.simulation_data
