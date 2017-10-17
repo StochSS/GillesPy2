4c3330b59d73f11076099bfc76400314cd793f4d
Dalton Nickerson <dnickers@unca.edu>
2017-10-16 20:05:50 -0400

    Using Numpy and pre-allocating memory.

---
+++ gillespy2/optimized_ssa_solver.py
@@ -0,0 +1,76 @@
+import gillespy2
+from gillespySolver import GillesPySolver
+import random
+import math
+import numpy as np
+
+class SSASolver(GillesPySolver):
+    """ TODO
+    """
+    
+    @classmethod
+    def run(self, model, t=20, number_of_trajectories=1,
+            increment=0.05, seed=None, debug=False, show_labels=False,stochkit_home=None):
+        self.simulation_data = []
+ 
+        curr_state = {}
+        propensity = {}
+        results = []
+        
+        for traj_num in range(number_of_trajectories):
+            trajectory = {}
+            results.append(trajectory)
+            for s in model.listOfSpecies:   #Initialize Species population
+                curr_state[s] = model.listOfSpecies[s].initial_value
+                trajectory[s]=np.zeros(shape=(int(t/increment)+1))
+            curr_state['vol'] = model.volume
+            trajectory['time'] = np.linspace(0,t,(t//increment+1))
+            curr_time = 0	  
+            entry_count = 0
+            for p in model.listOfParameters:
+                curr_state[p] = model.listOfParameters[p].value		
+		
+	     
+            while(entry_count < trajectory['time'].size):
+                prop_sum = 0
+                cumil_sum = 0
+                reaction = None
+                reaction_num = None
+                for r in model.listOfReactions:
+                    propensity[r] = eval(model.listOfReactions[r].propensity_function, curr_state)
+                    prop_sum += propensity[r]
+                reaction_num = random.uniform(0,prop_sum)
+                for r in model.listOfReactions:
+                    cumil_sum += propensity[r]
+                    if(cumil_sum >= reaction_num):
+                        reaction = r
+                        break
+                if(prop_sum <= 0):
+                    while(entry_count < trajectory['time'].size):
+                        for s in model.listOfSpecies:
+                            trajectory[s][entry_count]=(curr_state[s])
+                        entry_count += 1
+                    break
+
+                tau = -1*math.log(random.random())/prop_sum
+                curr_time += tau
+                while(entry_count < trajectory['time'].size and curr_time >= trajectory['time'][entry_count]):
+                    for s in model.listOfSpecies:
+                        trajectory[s][entry_count]=(curr_state[s])
+                    entry_count += 1
+
+                for react in model.listOfReactions[reaction].reactants:
+                    curr_state[str(react)] -=  model.listOfReactions[reaction].reactants[react]
+                for prod in model.listOfReactions[reaction].products:
+                    curr_state[str(prod)] += model.listOfReactions[reaction].products[prod]
+        return results
+    def get_trajectories(self, outdir, debug=False, show_labels=False):
+        if show_labels:
+            return self.simulation_data
+       # else:
+            
+            #TODO: need to account for 'show_labels'
+
+
+
+
