b0a376ae507740fbb5889f3b3387ac1eb1b816b0
Dalton Nickerson <dalton.nickerson@gmail.com>
2018-03-26 13:56:27 -0400

    Revising template file for user simulations.

--- gillespy2/c_base/SimulationTemplate.cpp
+++ gillespy2/c_base/SimulationTemplate.cpp
@@ -1,18 +1,21 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <time.h>
 #include "model.h"
 #include "ssa.h"
 using namespace Gillespy;
 
+bool seed_time = true;
 
 //Default constants
+__DEFINE_CONSTANTS__
+/*
 const uint number_trajectories = 7;
 const uint number_timesteps = 101;
 const double end_time = 100;
-const int random_seed = 9001;
 const double volume = 1.0;
-
+int random_seed = 9001;
 
 //Species Init
 std :: string s_names[] = {"A","B","C","D"};
@@ -20,22 +23,25 @@ uint populations[] = {301, 120, 0, 0};
 //Reactions Init
 std :: string r_names[] = {"r1","r2","r3"};
 
-
 //User defined constants
 const double rate1 = 0.0017;
 const double rate2 = 0.5;
 const double rate3 = 0.1;
+*/
 
 class PropensityFunction : public IPropensityFunction{
 public:
   double evaluate(uint reaction_number, uint* state){
     switch(reaction_number){
+__DEFINE_PROPENSITY__
+    /*
     case 0:
       return rate1 * state[0] * state[1] / volume;
     case 1:
       return rate2 * state[2];
     case 2:
       return rate3 * state[2];
+    */
     default: //Error
       return -1;
     }
@@ -49,7 +55,9 @@ int main(){
   
   Model model(species_names, species_populations, reaction_names);
 
+__DEFINE_REACTIONS_
   //Begin reaction species changes
+  /*
   model.reactions[0].species_change[0] = -1;
   model.reactions[0].species_change[1] = -1;
   model.reactions[0].species_change[2] = 1;
@@ -61,10 +69,14 @@ int main(){
   model.reactions[2].species_change[1] = 1;
   model.reactions[2].species_change[2] = -1;
   model.reactions[2].species_change[3] = 1;
-
+  */
   //End reaction species changes
   model.update_affected_reactions();
-  
+
+ if(seed_time){
+   random_seed = time(NULL);
+ }
+ 
   IPropensityFunction *propFun = new PropensityFunction();
   Simulation simulation(&model, number_trajectories, number_timesteps, end_time, propFun, random_seed);
   ssa_direct(&simulation);
--- gillespy2/ssa_c_solver.py
+++ gillespy2/ssa_c_solver.py
@@ -1,17 +1,45 @@
 import gillespy2
 from .gillespySolver import GillesPySolver
 
+def write_constants(outfile, model, t, number_of_trajectories, increment, seed):
+    number_timesteps = int(t/increment)
+    outfile.write("""
+const uint number_trajectories = {0};
+const uint number_timesteps = {1};
+const double end_time = {2};
+const double volume = {3};
+int random_seed;
+""".format(number_of_trajectories, number_timesteps, end_time, model.volume))
+    #Write seed
+    if isinstance(seed, int):
+        outfile.write("random_seed = {};\nseed_time = false;\n".format(seed))        
+    #    Write model species names.
+    outfile.write("std :: string s_names[] = {");
+    for species in model.listOfSpecies:
+        outfile.write('"{}"'.format(species))
+    #    Write initial populations.
+         
+
+
 class SSACSolver(GillesPySolver):
     """TODO"""
     @classmethod
     def run(self, model, t=20, number_of_trajectories=1,
             increment=0.05, seed=None, debug=False, show_labels=False,stochkit_home=None):
-        #Write simulation C++ file.
-        #    Write constants.
-        #        Write model species names.
-        #        Write initial populations.
-        #        Write reaction species changes.
-        #    Write propensity function.
+        #Open up template file for reading.
+        with open('SimulationTemplate.cpp', 'r') as template:
+            #Write simulation C++ file.
+            template_keword = "__DEFINE_"
+            with open('UserSimulation.cpp', 'w') as outfile:
+                for line in template:
+                    if line.startswith(template_keyword):
+                        line = line[len(template_keyword):]
+                        if line.startswith("CONSTANTS"):
+                            write_constants(outfile, model, t, number_of_trajectories, increment, seed)
+                    else:
+                        outfile.write(line)
+                #Write propensity function.
+                pass
         #Use makefile.
         #Execute simulation.
         #Parse/return results.
