44128e93506b175b8f8944a0a88771365984b209
Dalton Nickerson <dalton.nickerson@gmail.com>
2018-03-26 01:02:10 -0400

    Added imports for Numpy optimized SSA.

--- gillespy2/__init__.py
+++ gillespy2/__init__.py
@@ -1,3 +1,5 @@
 from .gillespy2 import *
 from .gillespySolver import *
 from .basic_ssa_solver import BasicSSASolver
+from .optimized_ssa_solver import SSASolver
+from .ssa_c_solver import SSACSolver
