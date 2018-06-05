eb4be87368f4b6315edecf2f9e8b0466a5e752b6
Dalton Nickerson <dalton.nickerson@gmail.com>
2018-06-05 08:45:02 -0400

    Added test cases for constructor and directory error.

--- test/test_c_solver.py
+++ test/test_c_solver.py
@@ -1,9 +1,19 @@
 import unittest
-from gillespy2.example_models import *
+import tempfile
+from gillespy2 import SSACSolver
+from gillespyError import SolverError, DirectoryError, BuildError, ExecutionError
+from gillespy2.example_models import Example
 
 class TestSSACSolver(unittest.TestCase):
     def test_create(self):
-        pass
+        model = Example()
+        solver = SSACSolver(model)
+    
+    def test_directory_exists(self):
+        with self.assertRaises(DirectoryError):
+            directory = tempfile.TemporaryDirectory()
+            model = Example()
+            solver = SSACSolver(model, directory)
 
 
 if __name__ == '__main__':
