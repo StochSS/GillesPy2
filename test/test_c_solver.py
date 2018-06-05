35d59c2856519d13379d06d3b8e56458d7041935
Dalton Nickerson <dalton.nickerson@gmail.com>
2018-06-04 23:16:18 -0400

    Created unit testing script for C solver.

---
+++ test/test_c_solver.py
@@ -0,0 +1,10 @@
+import unittest
+from gillespy2.example_models import *
+
+class TestSSACSolver(unittest.TestCase):
+    def test_create(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
