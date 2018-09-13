3bea0ce14b56b420d0f8fc6aedd2678d54c6f84b
Dalton <thisisnotdalton@gmail.com>
2018-09-13 13:55:31 -0400

    Simplified notebook with helper functions and cleaned up outputs/formatting.

--- test/test_c_solver.py
+++ test/test_c_solver.py
@@ -3,6 +3,7 @@ import tempfile
 from gillespy2 import SSACSolver, SolverError, DirectoryError, BuildError, ExecutionError
 from gillespy2.example_models import Example
 
+
 class TestSSACSolver(unittest.TestCase):
     def test_create(self):
         model = Example()
