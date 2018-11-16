80b522905fe6780da4acce8b422b1b99f98e7b72
Sean Matthew <smatthe2@unca.edu>
2018-11-16 14:07:57 -0500

    removed tau-leaping solver and root solver from project and from docs

--- gillespy2/solvers/numpy/__init__.py
+++ gillespy2/solvers/numpy/__init__.py
@@ -4,8 +4,6 @@ try:
     can_use_numpy = True
     from gillespy2.solvers.numpy.ssa_solver import NumPySSASolver
     from gillespy2.solvers.numpy.basic_ode_solver import BasicODESolver
-    from gillespy2.solvers.numpy.basic_root_solver import BasicRootSolver
-    from gillespy2.solvers.numpy.tau_leaping_solver import TauLeapingSolver
     from gillespy2.solvers.numpy.basic_tau_leaping_solver import BasicTauLeapingSolver
     from gillespy2.solvers.numpy.basic_tau_hybrid_solver import BasicTauHybridSolver
     log.debug("Successful Import of NumPy solvers.")
