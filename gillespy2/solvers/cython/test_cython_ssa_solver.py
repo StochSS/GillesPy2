c36eff19ce0684ca8e9d068b37c9436d31d60bc9
seanebum <smatthe2@unca.edu>
2018-12-20 12:29:02 -0500

    added unit tests for solvers

---
+++ gillespy2/solvers/cython/test_cython_ssa_solver.py
@@ -0,0 +1,16 @@
+import unittest
+import tempfile
+from gillespy2.core.gillespyError import SolverError, DirectoryError, BuildError, ExecutionError
+from gillespy2.example_models import Example
+from gillespy2.solvers.cython.cython_ssa_solver import CythonSSASolver
+
+
+class TestCythonSSASolver(unittest.TestCase):
+
+    def test_run_example(self):
+        model = Example()
+        results = model.run(solver=CythonSSASolver)
+
+
+if __name__ == '__main__':
+    unittest.main()
