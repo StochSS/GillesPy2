d63d48f6015011dccf4e17455a3dcd186e53996c
seanebum <smatthe2@unca.edu>
2019-01-15 12:44:31 -0500

    Moved unittest files to Test directory Fixed System Path for unittest files

--- test/test_basic_tau_hybrid_solver.py
+++ test/test_basic_tau_hybrid_solver.py
@@ -1,15 +1,17 @@
 import unittest
 import tempfile
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '..')))
 from gillespy2.core.gillespyError import SolverError, DirectoryError, BuildError, ExecutionError
 from gillespy2.example_models import Example
-from gillespy2.solvers.numpy.basic_ode_solver import BasicODESolver
+from gillespy2.solvers.numpy.basic_tau_hybrid_solver import BasicTauHybridSolver
 
 
-class TestBasicODESolver(unittest.TestCase):
+class TestBasicTauHybridSolver(unittest.TestCase):
 
     def test_run_example(self):
         model = Example()
-        results = model.run(solver=BasicODESolver)
+        results = model.run(solver=BasicTauHybridSolver)
 
 
 if __name__ == '__main__':
