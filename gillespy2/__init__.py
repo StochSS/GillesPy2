a5569353a4a8047eef77afc74df6a8f5d0ea9602
Brian Drawert <brian@drawert.net>
2019-01-30 07:45:34 -0500

    raise exception if run in python 2

--- gillespy2/__init__.py
+++ gillespy2/__init__.py
@@ -1,3 +1,8 @@
+import sys
+if (sys.version_info < (3,0)):
+    raise Exception("GillesPy2 only works in Python 3.0 and higher")
+
+
 from gillespy2.core import *
 import gillespy2.sbml
 import gillespy2.solvers
