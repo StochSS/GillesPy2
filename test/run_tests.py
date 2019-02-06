fa6200863aed0c578839b2f38d84166c1b6cd4f7
George M. Hall <georgemhall90@gmail.com>
2019-02-05 20:23:55 -0500

    Fixing tests to run under travis

--- test/run_tests.py
+++ test/run_tests.py
@@ -1,12 +1,17 @@
-import unittest
-import sys, os
+import unittest, sys, os
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '..')))
 
-loader = unittest.TestLoader()
-start_dir = '.'
-suite = loader.discover(start_dir)
+import test_empty_model, test_simple_model
 
 if __name__ == '__main__':
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
+    modules = [
+        test_empty_model,
+        test_simple_model
+        ]
 
+    for module in modules:
+        suite = unittest.TestLoader().loadTestsFromModule(module)
+        runner = unittest.TextTestRunner()
+        result = runner.run(suite)
+    
+    sys.exit(not result.wasSuccessful())
