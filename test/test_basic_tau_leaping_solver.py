8c29da51a7f816744c5a4a1d8deab7e12e17fc3a
Sean Matthew <smatthe2@unca.edu>
2019-05-17 11:26:32 -0400

    added unit tests for tau leaping solver return types

--- test/test_basic_tau_leaping_solver.py
+++ test/test_basic_tau_leaping_solver.py
@@ -5,10 +5,21 @@ from gillespy2.solvers.numpy.basic_tau_leaping_solver import BasicTauLeapingSolv
 
 class TestBasicTauLeapingSolver(unittest.TestCase):
 
-    def test_run_example(self):
-        model = Example()
-        results = model.run(solver=BasicTauLeapingSolver)
+    model = Example()
+    results = model.run(solver=BasicTauHybridSolver, show_labels=False)
+    labels_results = model.run(solver=BasicTauHybridSolver, show_labels=True)
 
+    def test_return_type(self):
+        assert(isinstance(self.results, list))
+        assert(isinstance(self.results[0], np.ndarray))
+        assert(isinstance(self.results[0][0], np.ndarray))
+        assert(isinstance(self.results[0][0][0], np.float))
+
+    def test_return_type_show_labels(self):
+        assert(isinstance(self.labels_results, list))
+        assert(isinstance(self.labels_results[0], dict))
+        assert(isinstance(self.labels_results[0]['Sp'], np.ndarray))
+        assert(isinstance(self.labels_results[0]['Sp'][0], np.float))
 
 if __name__ == '__main__':
     unittest.main()
