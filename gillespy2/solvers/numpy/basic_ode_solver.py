4efac948e3626c140d67212f9a21e25c14199718
Sean Matthew <smatthe2@unca.edu>
2019-05-17 12:26:34 -0400

    fixed issue where show labels was returning a dict of lists, rather than a dict of np.ndarrays

--- gillespy2/solvers/numpy/basic_ode_solver.py
+++ gillespy2/solvers/numpy/basic_ode_solver.py
@@ -69,16 +69,18 @@ class BasicODESolver(GillesPySolver):
         start_state = [model.listOfSpecies[species].initial_value for species in model.listOfSpecies]
         timeline = np.linspace(0, t, (t // increment + 1))
         result = odeint(BasicODESolver.rhs, start_state, timeline, args=(model,))
+        result = np.hstack((np.expand_dims(timeline, -1), result))
+        print(result)
 
         if show_labels:
             results_as_dict = {
                 'time': timeline
             }
             for i, species in enumerate(model.listOfSpecies):
-                results_as_dict[species] = [result[:, i]]
+                results_as_dict[species] = result[:, i]
+                print(results_as_dict[species])
             results = [results_as_dict] * number_of_trajectories
         else:
-            result = np.hstack((np.expand_dims(timeline, -1), result))
             results = np.stack([result] * number_of_trajectories, axis=0)
 
         return results
