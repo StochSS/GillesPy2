cdd885b075809362c63212e761a47db9f2b15555
Brian Drawert <brian@drawert.net>
2019-06-18 10:59:03 -0400

    note

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #pip3 install  coverage
 #pip3 install  coverage-badge
+#pip3 install python-libsbml
 
 coverage run --source=gillespy2 --omit=gillespy2/solvers/stochkit/* test/run_tests.py -m develop
 coverage html
