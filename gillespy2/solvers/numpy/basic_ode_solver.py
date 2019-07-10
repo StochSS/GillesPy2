b9e8318c27893a0520562658ead1504e2d90c8cc
Sean Matthew <smatthe2@unca.edu>
2019-07-10 14:26:41 -0400

    rebuild ode solver with scipy.integrate.ode instead of scipy.integrate.odeint, now works with vilar model

--- gillespy2/solvers/numpy/basic_ode_solver.py
+++ gillespy2/solvers/numpy/basic_ode_solver.py
@@ -1,6 +1,8 @@
 """GillesPy2 Solver for ODE solutions."""
 
+from scipy.integrate import ode
 from scipy.integrate import odeint
+from collections import OrderedDict
 import numpy as np
 from gillespy2.core import GillesPySolver
 from gillespy2.core import log
@@ -13,6 +15,23 @@ class BasicODESolver(GillesPySolver):
     name = "BasicODESolver"
 
     @staticmethod
+    def __f(t, y, curr_state, model, c_prop):
+        curr_state['t'] = t
+        state_change =  OrderedDict()
+        for i, species in enumerate(model.listOfSpecies):
+            curr_state[species] = y[i]
+            state_change[species] = 0
+        propensity = OrderedDict()
+        for r_name, reaction in model.listOfReactions.items():
+            propensity[r_name] = eval(c_prop[r_name], curr_state)
+            for react, stoich in reaction.reactants.items():
+                state_change[str(react)] -= propensity[r_name] * stoich
+            for prod, stoich in reaction.products.items():
+                state_change[str(prod)] += propensity[r_name] * stoich
+        state_change = list(state_change.values())
+        return state_change
+
+    @staticmethod
     def rhs(start_state, time, model):
         """
         The right hand side of the differential equation, uses scipy.integrate odeint
@@ -22,14 +41,14 @@ class BasicODESolver(GillesPySolver):
         :return: integration step
         """
         curr_state = {}
-        state_change = {}
+        state_change = []
         curr_state['vol'] = model.volume
 
         for i, species in enumerate(model.listOfSpecies):
             curr_state[species] = start_state[i]
             state_change[species] = 0
 
-        for parameter in model.listOfParameters:
+        for p_name, parameter in model.listOfParameters:
             curr_state[parameter] = model.listOfParameters[parameter].value
 
         propensity = {}
@@ -68,9 +87,55 @@ class BasicODESolver(GillesPySolver):
             log.warning("Generating duplicate trajectories for model with ODE Solver. Consider running with only 1 trajectory.")
 
         start_state = [model.listOfSpecies[species].initial_value for species in model.listOfSpecies]
+
+        # create mapping of species dictionary to array indices
+        species_mappings = model.sanitized_species_names()
+        species = list(species_mappings.keys())
+        parameter_mappings = model.sanitized_parameter_names()
+        number_species = len(species)
+
+        # create numpy array for timeline
         timeline = np.linspace(0, t, (t // increment + 1))
-        result = odeint(BasicODESolver.rhs, start_state, timeline, args=(model,), mxstep=max_steps)
-        result = np.hstack((np.expand_dims(timeline, -1), result))
+
+        # create numpy matrix to mark all state data of time and species
+        trajectory_base = np.empty((number_of_trajectories, timeline.size, number_species + 1))
+
+        # copy time values to all trajectory row starts
+        trajectory_base[:, :, 0] = timeline
+
+        # copy initial populations to base
+        for i, s in enumerate(species):
+            trajectory_base[:, 0, i + 1] = model.listOfSpecies[s].initial_value
+
+        # compile reaction propensity functions for eval
+        c_prop = OrderedDict()
+        for r_name, reaction in model.listOfReactions.items():
+            c_prop[r_name] = compile(reaction.ode_propensity_function, '<string>', 'eval')
+
+        result = trajectory_base[0]
+        curr_time = 0
+        entry_count = 0
+
+        y0 = [0] * len(model.listOfSpecies)
+        curr_state = OrderedDict()
+        for i, s in enumerate(model.listOfSpecies.values()):
+            curr_state[s.name] = s.initial_value
+            y0[i] = s.initial_value
+        for p_name, param in model.listOfParameters.items():
+            curr_state[p_name] = param.value
+        rhs = ode(BasicODESolver.__f).set_integrator('lsoda')
+        rhs.set_initial_value(y0, curr_time).set_f_params(curr_state, model, c_prop)
+
+        while entry_count < timeline.size - 1:
+            int_time = curr_time + increment
+            entry_count += 1
+            y0 = rhs.integrate(int_time)
+            curr_time += increment
+            for i, spec in enumerate(model.listOfSpecies):
+                curr_state[spec] = y0[i]
+                result[entry_count][i+1] = curr_state[spec]
+        #result = odeint(BasicODESolver.rhs, start_state, timeline, args=(model,), mxstep=max_steps)
+        #result = np.hstack((np.expand_dims(timeline, -1), result))
 
         if show_labels:
             results_as_dict = {
