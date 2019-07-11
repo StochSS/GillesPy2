adf9768827177621e11c3384a63c1fbbac55c510
Sean Matthew <smatthe2@unca.edu>
2019-07-11 13:55:53 -0400

    re-added empty tests for solvers... original test bodies were moved to test_all_solvers.  Also, re-added rate rule testing for the tau hybrid solver, and re-included it in run_tests.py

--- test/run_tests.py
+++ test/run_tests.py
@@ -19,6 +19,7 @@ if __name__ == '__main__':
     import test_empty_model
     import test_model
     import test_ode_solver
+    import test_tau_hybrid_solver
     import test_simple_model
     import test_ssa_c_solver
     import test_SBML
@@ -29,6 +30,7 @@ if __name__ == '__main__':
         test_empty_model,
         test_model,
         test_ode_solver,
+        test_tau_hybrid_solver,
         test_simple_model,
         test_ssa_c_solver,
         test_SBML,
---
+++ test/test_tau_leaping_solver.py
@@ -0,0 +1,12 @@
+import unittest
+import numpy as np
+from gillespy2.example_models import Example
+from gillespy2.solvers.numpy.ssa_solver import NumPySSASolver
+
+
+class TestNumPySSASolver(unittest.TestCase):
+    model = Example()
+    
+
+if __name__ == '__main__':
+    unittest.main()
