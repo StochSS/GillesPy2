9a901a23460ebb14c636c5cc54ceee317ed14d80
Michael Hucka <mhucka@caltech.edu>
2019-08-23 14:53:33 -0700

    Pull version and other data from __version__.py

--- setup.py
+++ setup.py
@@ -1,18 +1,55 @@
+#!/usr/bin/env python3
+# =============================================================================
+# @file    setup.py
+# @brief   GillesPy2 setup file
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/GillesPy2/GillesPy2
+#
+# Note: how to do a PyPI release
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Run the following commands:
+#
+#   python3 setup.py sdist bdist_wheel
+#   twine upload dist/*
+#
+# =============================================================================
+
 from setuptools import setup, find_packages
 from setuptools.command.develop import develop
 from setuptools.command.install import install
 from setuptools.command.bdist_egg import bdist_egg
 from setuptools.command.easy_install import easy_install
 import os
+from os import path
 
-##########################################################
-### HOW TO DO A PYPI RELEASE ###
-# python3 setup.py sdist bdist_wheel
-# twine upload dist/* 
-##########################################################
 
-SETUP_DIR = os.path.dirname(os.path.abspath(__file__))
+# Read the contents of auxiliary files.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+SETUP_DIR = path.dirname(os.path.abspath(__file__))
+
+with open(path.join(SETUP_DIR, 'requirements.txt')) as f:
+    reqs = f.read().rstrip().splitlines()
+
+with open("README.md", "r", errors = 'ignore') as f:
+    readme = f.read()
+
+# The following reads the variables without doing an "import handprint",
+# because the latter will cause the python execution environment to fail if
+# any dependencies are not already installed -- negating most of the reason
+# we're using setup() in the first place.  This code avoids eval, for security.
+
+version = {}
+with open(path.join(SETUP_DIR, 'gillespy2/__version__.py')) as f:
+    text = f.read().rstrip().splitlines()
+    vars = [line for line in text if line.startswith('__') and '=' in line]
+    for v in vars:
+        setting = v.split('=')
+        version[setting[0].strip()] = setting[1].strip().replace("'", '')
+
+
+# Decorate some command classes used below.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def stoch_path(command_subclass):
     """
@@ -29,7 +66,6 @@ def stoch_path(command_subclass):
     return command_subclass
 
 
-# update all install classes with our new class
 @stoch_path
 class develop_new(develop):
     pass
@@ -50,35 +86,36 @@ class easy_install_new(easy_install):
     pass
 
 
-with open('README.md', 'r') as fh:
-    full_description = fh.read()
+# Finally, define our namesake.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-setup(name="gillespy2",
-      version="1.1.2",
-      packages=find_packages('.'),
-      include_package_data=True,
-      description='Python interface for Gillespie style biochemical simulations',
-      long_description=full_description,
-      long_description_content_type="text/markdown",
+setup(name             = version['__title__'].lower(),
+      version          = version['__version__'],
+      author           = version['__author__'],
+      author_email     = version['__email__'],
+      maintainer       = version['__author__'],
+      maintainer_email = version['__email__'],
+      license          = version['__license__'],
+      url              = version['__url__'],
+      download_url     = version['__download_url__'],
+      description      = version['__description__'],
+      long_description = readme,
+      long_description_content_type = "text/markdown",
+      keywords         = "gillespy2, Gillespie algorithm, biochemical simulation",
+      project_urls     = {
+          "Tracker": "https://github.com/GillesPy2/GillesPy2/issues",
+          "Source" : "https://github.com/GillesPy2/GillesPy2",
+      },
 
-      install_requires=["numpy",
-                        "matplotlib",
-                        "scipy"],
+      packages         = find_packages('.'),
+      include_package_data = True,
+      install_requires = reqs,
+      cmdclass         = {'bdist_egg': bdist_egg_new,
+                          'install': install_new,
+                          'develop': develop_new,
+                          'easy_install': easy_install_new},
 
-      author="Brian Drawert, Kevin Sanft, Sean Matthew, George Hall, Dalton Nickerson, Samuel Hodges, Emma Weisgerber, Eliot Dixon, Ghilman Brock, W.R. Jackson",
-      author_email="bdrawert@unca.edu",
-      license="GPL",
-      keywords="gillespy2, gillespie algorithm, biochemical simulation",
-
-      url="https://gillespy2.github.io/GillesPy2/",
-
-      download_url="https://github.com/GillesPy2/GillesPy2/tarball/master/",
-
-      cmdclass={'bdist_egg': bdist_egg_new,
-                'install': install_new,
-                'develop': develop_new,
-                'easy_install': easy_install_new},
-      classifiers=[
+      classifiers      = [
           'Development Status :: 5 - Production/Stable',
           'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
           'Operating System :: OS Independent',
@@ -89,5 +126,4 @@ setup(name="gillespy2",
           'Topic :: Scientific/Engineering :: Medical Science Apps.',
           'Intended Audience :: Science/Research'
       ],
-
-      )
+)
