91211e1a077127fb80b50b18025ab1052f570012
Michael Hucka <mhucka@caltech.edu>
2019-08-23 14:43:12 -0700

    Add __version__, __author__, and other module-level dunder names
    Following examples such this code snippet from PEP 8,
    https://www.python.org/dev/peps/pep-0008/#module-level-dunder-names
    and some frequently-referenced long-ago guidelines such as
    http://web.archive.org/web/20111010053227/http://jaynes.colorado.edu/PythonGuidelines.html
    which still get referenced by Stack Overflow answers such as
    https://stackoverflow.com/a/1523456/743730
    this commit adds __version__, __author__, __license__, __url__,
    and other often-used attributes at the module level, so that
    callers can do reference like "GillesPy2.__version__".

---
+++ gillespy2/__version__.py
@@ -0,0 +1,16 @@
+# =============================================================================
+# @file    __version__.py
+# @brief   GillesPy2 version info
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/GillesPy2/GillesPy2
+# =============================================================================
+
+__version__      = '1.1.2'
+__title__        = 'GillesPy2'
+__description__  = 'Python interface for Gillespie-style biochemical simulations'
+__url__          = 'https://github.com/GillesPy2/GillesPy2'
+__download_url__ = 'https://github.com/GillesPy2/GillesPy2/tarball/master/'
+__author__       = 'Brian Drawert, Kevin Sanft, Sean Matthew, George Hall, Dalton Nickerson, Samuel Hodges, Emma Weisgerber, Eliot Dixon, Ghilman Brock, W.R. Jackson'
+__email__        = 'bdrawert@unca.edu'
+__license__      = 'GPL'
+__copyright__    = 'Copyright (C) 2017-2019'
