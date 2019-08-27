8911f2c08eb14cd93534fcab2b5e4ff4cd633f26
Michael Hucka <mhucka@caltech.edu>
2019-08-27 10:19:19 -0700

    Remove no longer needed code

--- setup.py
+++ setup.py
@@ -48,72 +48,29 @@ with open(path.join(SETUP_DIR, 'gillespy2/__version__.py')) as f:
         version[setting[0].strip()] = setting[1].strip().replace("'", '')
 
 
-# Decorate some command classes used below.
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-def stoch_path(command_subclass):
-    """
-    A decorator for classes subclassing one of the setuptools commands.
-    It modifies the run() method.
-    """
-    orig_run = command_subclass.run
-
-    def modified_run(self):
-        success = False
-        orig_run(self)
-
-    command_subclass.run = modified_run
-    return command_subclass
-
-
-@stoch_path
-class develop_new(develop):
-    pass
-
-
-@stoch_path
-class install_new(install):
-    pass
-
-
-@stoch_path
-class bdist_egg_new(bdist_egg):
-    pass
-
-
-@stoch_path
-class easy_install_new(easy_install):
-    pass
-
-
 # Finally, define our namesake.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-setup(name             = version['__title__'].lower(),
-      version          = version['__version__'],
-      author           = version['__author__'],
-      author_email     = version['__email__'],
-      maintainer       = version['__author__'],
-      maintainer_email = version['__email__'],
-      license          = version['__license__'],
-      url              = version['__url__'],
-      download_url     = version['__download_url__'],
-      description      = version['__description__'],
-      long_description = readme,
+setup(name                 = version['__title__'].lower(),
+      version              = version['__version__'],
+      author               = version['__author__'],
+      author_email         = version['__email__'],
+      maintainer           = version['__author__'],
+      maintainer_email     = version['__email__'],
+      license              = version['__license__'],
+      url                  = version['__url__'],
+      download_url         = version['__download_url__'],
+      description          = version['__description__'],
+      long_description     = readme,
       long_description_content_type = "text/markdown",
-      keywords         = "gillespy2, Gillespie algorithm, biochemical simulation",
-      project_urls     = {
+      keywords             = "biochemical simulation, Gillespie algorithm, stochastic simulation, biology",
+      project_urls         = {
           "Tracker": "https://github.com/GillesPy2/GillesPy2/issues",
           "Source" : "https://github.com/GillesPy2/GillesPy2",
       },
-
-      packages         = find_packages('.'),
+      packages             = find_packages('.'),
       include_package_data = True,
-      install_requires = reqs,
-      cmdclass         = {'bdist_egg': bdist_egg_new,
-                          'install': install_new,
-                          'develop': develop_new,
-                          'easy_install': easy_install_new},
+      install_requires     = reqs,
 
       classifiers      = [
           'Development Status :: 5 - Production/Stable',
