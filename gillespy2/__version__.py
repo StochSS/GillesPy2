3ea5b856be544fe86d6927c4f462c80bf23bca76
Brian Drawert <brian@drawert.net>
2019-08-29 17:06:12 -0400

    version 1.2.1

--- gillespy2/__version__.py
+++ gillespy2/__version__.py
@@ -5,7 +5,7 @@
 # @website https://github.com/GillesPy2/GillesPy2
 # =============================================================================
 
-__version__      = '1.2'
+__version__      = '1.2.1'
 __title__        = 'GillesPy2'
 __description__  = 'Python interface for Gillespie-style biochemical simulations'
 __url__          = 'https://github.com/GillesPy2/GillesPy2'
