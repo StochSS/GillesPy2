Fix for issue #225: calculation of timeline
The use of floor division in calculating the number of timesteps can
result in off-by-1 errors compared to the expected number of
timesteps.  Using round() solves this, at least in my testing.

gillespy2/solvers/numpy/basic_tau_leaping_solver.py
@@ -110,7 +110,7 @@ class BasicTauLeapingSolver(GillesPySolver):
                 raise ModelError('seed must be a positive integer')
 
         # create numpy array for timeline
-        timeline = np.linspace(0, t, (t // increment + 1))
+        timeline = np.linspace(0, t, round(t / increment + 1))
 
         # create numpy matrix to mark all state data of time and species
         trajectory_base = np.empty((number_of_trajectories, timeline.size, number_species + 1))