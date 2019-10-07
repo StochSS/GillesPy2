af0229a25134b322ecd856b339ea9bbeb75856ff
Michael Hucka <mhucka@caltech.edu>
2019-10-07 14:39:57 -0700

    Fix bug in __eq__ definition of SortableObject

--- gillespy2/core/gillespy2.py
+++ gillespy2/core/gillespy2.py
@@ -50,7 +50,7 @@ class SortableObject(object):
 
     def __eq__(self, other):
         return (isinstance(other, self.__class__)
-                and ordered(self) == ordered(other))
+                and self.__dict__ == other.__dict__)
 
     def __ne__(self, other):
         return not self.__eq__(other)
