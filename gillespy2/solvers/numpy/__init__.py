3b0d09d98af7744b714f63f7c4d0d2b1516dea23
seanebum <smatthe2@unca.edu>
2020-01-06 11:17:24 -0500

    fixed improper import reference causing get_best_ssa_solver() to omit NumPy

--- gillespy2/solvers/numpy/__init__.py
+++ gillespy2/solvers/numpy/__init__.py
@@ -5,11 +5,11 @@ try:
     from gillespy2.solvers.numpy.ssa_solver import NumPySSASolver
     from gillespy2.solvers.numpy.basic_ode_solver import BasicODESolver
     from gillespy2.solvers.numpy.basic_tau_leaping_solver import BasicTauLeapingSolver
-    from gillespy2.solvers.numpy.basic_hybrid_solver import BasicHybridSolver
+    from gillespy2.solvers.numpy.basic_tau_hybrid_solver import BasicTauHybridSolver
     log.debug("Successful Import of NumPy solvers.")
 except Exception as e:
     log.warn(" Unable to use NumPy: {0}. The performance of this package can be significantly increased if you install NumPy.".format(e))
     can_use_numpy = False
 
 
-__all__ = ['NumPySSASolver', 'BasicODESolver', 'BasicTauLeapingSolver', 'BasicHybridSolver'] if can_use_numpy else []
+__all__ = ['NumPySSASolver', 'BasicODESolver', 'BasicTauLeapingSolver', 'BasicTauHybridSolver'] if can_use_numpy else []
