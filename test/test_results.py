e2049ab3348954517863f79cb4a3df0172ee6796
Jesse Reeve <enszyme@gmail.com>
2020-03-18 17:25:51 -0400

    Added increased unit test coverage. Still need coverage for line 273 of results.py - this should be covered by test_to_csv_ensemble_result_no_nametag() in test_results.py

--- test/test_results.py
+++ test/test_results.py
@@ -40,6 +40,37 @@ class TestResults(unittest.TestCase):
             test_path = tempdir+"/"+test_nametag+test_stamp+"/"+test_nametag+".csv"
             assert os.path.isfile(test_path)
 
+    def test_to_csv_single_result_no_stamp(self):
+        test_data = {'time':[0]}
+        result = Results(data=test_data)
+        test_nametag = "test_nametag"
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            result.to_csv(nametag=test_nametag, path=tempdir)
+            assert len(os.listdir(tempdir)) is not 0
+
+    def test_to_csv_single_result_no_nametag(self):
+        test_data = {'time':[0]}
+        test_model = Model('test_model')
+        result = Results(data=test_data,model=test_model)
+        result.solver_name = 'test_solver'
+        test_stamp = "test_stamp"
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            result.to_csv(stamp=test_stamp, path=tempdir)
+            assert len(os.listdir(tempdir)) is not 0
+
+    def test_to_csv_single_result_no_path(self):
+        test_data = {'time':[0]}
+        result = Results(data=test_data)
+        test_nametag = "test_nametag"
+        test_stamp = "test_stamp"
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            os.chdir(tempdir)
+            result.to_csv(stamp=test_stamp, nametag=test_nametag)
+            assert len(os.listdir(tempdir)) is not 0
+
     def test_to_csv_ensemble_result_no_data(self):
         result = EnsembleResults(data=None)
         test_nametag = "test_nametag"
@@ -82,6 +113,38 @@ class TestResults(unittest.TestCase):
             result.to_csv(stamp = test_stamp, nametag = test_nametag, path=tempdir)
             test_path = tempdir+"/"+test_nametag+test_stamp+"/"+test_nametag+"0.csv"
             assert os.path.isfile(test_path)
+
+    def test_to_csv_ensemble_result_no_stamp(self):
+        test_data = {'time':[0]}
+        result = EnsembleResults(data=[test_data,test_data])
+        test_nametag = "test_nametag"
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            result.to_csv(nametag=test_nametag, path=tempdir)
+            assert len(os.listdir(tempdir)) is not 0
+
+    def test_to_csv_ensemble_result_no_nametag(self):
+        test_data = {'time':[0]}
+        test_model = Model('test_model')
+        result = Results(data=test_data,model=test_model)
+        ensemble_result = EnsembleResults(data=[result,result])
+        result.solver_name = 'test_solver'
+        test_stamp = "test_stamp"
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            result.to_csv(stamp=test_stamp, path=tempdir,nametag=None)
+            assert len(os.listdir(tempdir)) is not 0
+
+    def test_to_csv_ensemble_result_no_path(self):
+        test_data = {'time':[0]}
+        result = EnsembleResults(data=[test_data,test_data])
+        test_nametag = "test_nametag"
+        test_stamp = "test_stamp"
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            os.chdir(tempdir)
+            result.to_csv(stamp=test_stamp, nametag=test_nametag)
+            assert len(os.listdir(tempdir)) is not 0
 
 if __name__ == '__main__':
     unittest.main()
