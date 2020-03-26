de0be18cb3df701361f24f63f4165414dd2b916a
Momentopolis <eweisger@unca.edu>
2020-03-26 15:36:36 -0400

    Removed print statement in test_simple_model that was showing up when running the tests. Also uncommented the example model tests since the divide by zero error is no longer an issue

--- test/test_example_models.py
+++ test/test_example_models.py
@@ -5,29 +5,29 @@ from TestBattery import *
 
 class TestExampleModels(unittest.TestCase):
 
-    #def test_trichloroethylene_example(self):
-    #    trichloroethylene_model = Trichloroethylene()
-     #   results = trichloroethylene_model.run()
+    def test_trichloroethylene_example(self):
+        trichloroethylene_model = Trichloroethylene()
+        results = trichloroethylene_model.run()
 
-    #def test_lacOperon_example(self):
-     #   lacOperon_model = LacOperon()
-      #  results = lacOperon_model.run(solver=BasicODESolver)
+    def test_lacOperon_example(self):
+        lacOperon_model = LacOperon()
+        results = lacOperon_model.run(solver=BasicODESolver)
 
-    #def test_schlogl_example(self):
-     #   schlogl_model = Schlogl()
-      #  results = schlogl_model.run()
+    def test_schlogl_example(self):
+        schlogl_model = Schlogl()
+        results = schlogl_model.run()
 
-    #def test_michaelisMenten_example(self):
-     #   michaelisMenten_model = MichaelisMenten()
-      #  results = michaelisMenten_model.run()
+    def test_michaelisMenten_example(self):
+        michaelisMenten_model = MichaelisMenten()
+        results = michaelisMenten_model.run()
 
-    #def test_toggleSwitch_example(self):
-     #   toggleSwitch_model = ToggleSwitch()
-      #  results = toggleSwitch_model.run()
+    def test_toggleSwitch_example(self):
+        toggleSwitch_model = ToggleSwitch()
+        results = toggleSwitch_model.run()
 
-    #def test_example_example(self):
-     #   example_model = Example()
-      #  results = example_model.run()
+    def test_example_example(self):
+        example_model = Example()
+        results = example_model.run()
 
     def test_tyson2StateOscillator_example(self):
         tyson2StateOscillator_model = Tyson2StateOscillator()
