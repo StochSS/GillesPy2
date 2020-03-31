bf9c56a333ca89ec9765f1917a4c7ffaeaa879b1
Momentopolis <eweisger@unca.edu>
2020-03-31 16:20:14 -0400

    Changed run_coverage.sh so it does not include the cython solver files

--- run_coverage.sh
+++ run_coverage.sh
@@ -3,5 +3,5 @@
 #pip3 install  coverage-badge
 #pip3 install python-libsbml
 
-coverage run --source=gillespy2 --omit=gillespy2/solvers/stochkit/* test/run_tests.py -m develop
+coverage run --source=gillespy2 --omit=*gillespy2/solvers/stochkit/*,*gillespy2/solvers/cython/* test/run_tests.py -m develop
 coverage html
