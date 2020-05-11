7629627b3637f273a86a3de8d501853ada9a6144
seanebum <smatthe2@unca.edu>
2020-05-11 13:04:00 -0400

    replaced stat dict with numpy array, and used that to create functions instead of using eval

--- gillespy2/solvers/numpy/basic_ode_solver.py
+++ gillespy2/solvers/numpy/basic_ode_solver.py
@@ -1,8 +1,8 @@
 """GillesPy2 Solver for ODE solutions."""
 
 from threading import Thread, Event
+from types import FunctionType
 from scipy.integrate import ode
-from scipy.integrate import odeint
 from collections import OrderedDict
 import numpy as np
 from gillespy2.core import GillesPySolver, log
@@ -24,7 +24,7 @@ class BasicODESolver(GillesPySolver):
         result = None
 
     @staticmethod
-    def __f(t, y, curr_state, model, c_prop):
+    def __f(t, y, propensity, sm, propensities):
         """
         The right hand side of the differential equation, uses scipy.integrate odeint
         :param t: time as a numpy array
@@ -34,19 +34,8 @@ class BasicODESolver(GillesPySolver):
         :param c_prop: precompiled reaction propensity function
         :return: integration step
         """
-        curr_state['t'] = t
-        state_change =  OrderedDict()
-        for i, species in enumerate(model.listOfSpecies):
-            curr_state[species] = y[i]
-            state_change[species] = 0
-        propensity = OrderedDict()
-        for r_name, reaction in model.listOfReactions.items():
-            propensity[r_name] = eval(c_prop[r_name], curr_state)
-            for react, stoich in reaction.reactants.items():
-                state_change[react.name] -= propensity[r_name] * stoich
-            for prod, stoich in reaction.products.items():
-                state_change[prod.name] += propensity[r_name] * stoich
-        state_change = list(state_change.values())
+        for i in range(len(propensity)): propensities[i] = propensity[i](y)
+        state_change = np.multiply(sm, propensities).sum(axis=0)
         return state_change
 
     @classmethod
@@ -59,7 +48,7 @@ class BasicODESolver(GillesPySolver):
         :param t: end time of simulation
         :param number_of_trajectories: Should be 1.
             This is deterministic and will always have same results
-        :param increment: time step increment for plotting
+        :param dt: time step increment for plotting
         :param show_labels: If true, simulation returns a list of trajectories, where each list entry is a dictionary containing key value pairs of species : trajectory.  If false, returns a numpy array with shape [traj_no, time, species]
         :param integrator: integrator to be used form scipy.integrate.ode. Options include 'vode', 'zvode', 'lsoda', 'dopri5', and 'dop835'.  For more details, see https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.ode.html
         :param integrator_options: a dictionary containing options to the scipy integrator. for a list of options, see https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.ode.html.
@@ -78,7 +67,7 @@ class BasicODESolver(GillesPySolver):
             log.warning("Generating duplicate trajectories for model with ODE Solver. Consider running with only 1 trajectory.")
         sim_thread = Thread(target=self.___run, args=(model,), kwargs={'t':t,
                                         'number_of_trajectories':number_of_trajectories,
-                                        'increment':increment, 'show_labels':show_labels, 
+                                        'dt':increment, 'show_labels':show_labels, 
                                         'timeout':timeout,
                                         'integrator':integrator,
                                         'integrator_options':integrator_options})
@@ -93,29 +82,51 @@ class BasicODESolver(GillesPySolver):
             raise self.has_raised_exception
         return self.result, self.rc
 
-    def ___run(self, model, t=20, number_of_trajectories=1, increment=0.05, timeout=None,
+    def ___run(self, model, t=20, number_of_trajectories=1, dt=0.05, timeout=None,
             show_labels=True, integrator='lsoda', integrator_options={}, **kwargs):
         try:
-            self.__run(model, t, number_of_trajectories, increment, timeout,
+            self.__run(model, t, number_of_trajectories, dt, timeout,
                         show_labels, integrator, integrator_options, **kwargs)
         except Exception as e:
             self.has_raised_exception = e
             self.result = []
             return [], -1
 
-    def __run(self, model, t=20, number_of_trajectories=1, increment=0.05, timeout=None,
+    def __run(self, model, t=20, number_of_trajectories=1, dt=0.05, timeout=None,
             show_labels=True, integrator='lsoda', integrator_options={}, **kwargs):
 
-        start_state = [model.listOfSpecies[species].initial_value for species in model.listOfSpecies]
-
-        # create mapping of species dictionary to array indices
-        species_mappings = model.sanitized_species_names()
+        # create mappings to array indices
+        species_mappings = model._listOfSpecies
         species = list(species_mappings.keys())
-        parameter_mappings = model.sanitized_parameter_names()
+        parameter_mappings = model._listOfParameters
+        parameters = list(parameter_mappings.keys())
         number_species = len(species)
+        number_parameters = len(parameters)
+        reactions = model._listOfReactions
+        number_reactions = len(reactions)
+
+        vr = [''] * number_reactions
+        propensity = [0] * number_reactions
+        sm = np.zeros((number_reactions, number_species+number_parameters))
+        for i, r in enumerate(reactions.values()):
+            vr[i] = r.propensity_function.replace('vol', str(model.volume))
+            for j, s in reversed(list(enumerate(species_mappings.values()))):
+                if s in r.reactants:
+                    sm[i, j] -= r.reactants[s]
+                if s in r.products:
+                    sm[i, j] += r.products[s]
+                if s in vr[i]:
+                    vr[i] = vr[i].replace(s, 'y[{}]'.format(j))
+            for j, p in reversed(list(enumerate(parameter_mappings.values()))):
+                vr[i] = vr[i].replace(p, 'y[{}]'.format(j+number_species))
+        for i in range(number_reactions):
+            pcode = compile('def pf(y): return {}'.format(vr[i]), '<string>', 'exec')
+            pfunc = FunctionType(pcode.co_consts[0], globals(), 'pf')
+            propensity[i] = pfunc
+
 
         # create numpy array for timeline
-        timeline = np.linspace(0, t, int(round(t / increment + 1)))
+        timeline = np.linspace(0, t, int(round(t / dt + 1)))
 
         # create numpy matrix to mark all state data of time and species
         trajectory_base = np.zeros((number_of_trajectories, timeline.size, number_species + 1))
@@ -127,42 +138,33 @@ class BasicODESolver(GillesPySolver):
         for i, s in enumerate(species):
             trajectory_base[:, 0, i + 1] = model.listOfSpecies[s].initial_value
 
-        # compile reaction propensity functions for eval
-        c_prop = OrderedDict()
-        for r_name, reaction in model.listOfReactions.items():
-            c_prop[r_name] = compile(reaction.ode_propensity_function, '<string>', 'eval')
-
         result = trajectory_base[0]
-        curr_time = 0
-        entry_count = 0
+        t0 = 0
+        y0 = np.zeros(len(species)+len(parameters))
 
-        y0 = [0] * len(model.listOfSpecies)
-        curr_state = OrderedDict()
-        for i, s in enumerate(model.listOfSpecies.values()):
-            curr_state[s.name] = s.initial_value
-            y0[i] = s.initial_value
-        for p_name, param in model.listOfParameters.items():
-            curr_state[p_name] = param.value
+        for i, s in enumerate(species):
+            y0[i] = model.listOfSpecies[s].initial_value
+        for i, p in enumerate(parameters):
+            y0[i+len(species)] = model.listOfParameters[p].value
+
+        propensities = np.zeros((len(vr), 1))
         rhs = ode(BasicODESolver.__f).set_integrator(integrator, **integrator_options)
-        rhs.set_initial_value(y0, curr_time).set_f_params(curr_state, model, c_prop)
+        rhs.set_initial_value(y0, t0).set_f_params(propensity, sm, propensities)
+        entry_count = 0
 
         while entry_count < timeline.size - 1:
             if self.stop_event.is_set():
                 self.rc = 33
                 break
-            int_time = curr_time + increment
             entry_count += 1
-            y0 = rhs.integrate(int_time)
-            curr_time += increment
-            for i, spec in enumerate(model.listOfSpecies):
-                curr_state[spec] = y0[i]
-                result[entry_count][i+1] = curr_state[spec]
+            y = rhs.integrate(rhs.t+dt)
+            result[entry_count][1:] = y[:number_species]
 
         if show_labels:
             results_as_dict = {
                 'time': timeline
             }
-            for i, species in enumerate(model.listOfSpecies):
+            for i, species in enumerate(species):
                 results_as_dict[species] = result[:, i+1]
             results = [results_as_dict] * number_of_trajectories
         else:
