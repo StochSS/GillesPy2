6c3cc469545544fcb5cd63ff2126602dc310bd36
Fin Carter <fcarter@unca.edu>
2020-06-16 13:29:22 -0400

    - Remove print line from ssa_solver.py - Add dependency graphing solution for ssa_c_solver.py and variable_ssa_c_solver.py - Fix way dependencies were calculated in numpyutilities.py - change name from numpyutilities.py to utilities.py - Add unit tests in test_propensity_parser.py

--- gillespy2/solvers/utilities/utilities.py
+++ gillespy2/solvers/utilities/utilities.py
@@ -2,8 +2,15 @@ import ast
 import gillespy2
 
 
-def species_parse(model,custom_prop_fun):
+def species_parse(model, custom_prop_fun):
+    """
+    This function uses Pythons AST module to parse custom propensity function, looking for Species in a model
+    :param model: Model to be checked for species
+    :param custom_prop_fun: The custom propensity function to be parsed
+    :return: List of species objects that are found in a custom propensity function
+    """
     parsed_species = []
+
     class SpeciesParser(ast.NodeTransformer):
         def visit_Name(self, node):
             if isinstance(model.get_element(node.id),gillespy2.core.Species):
@@ -11,12 +18,19 @@ def species_parse(model,custom_prop_fun):
 
 
     expr = custom_prop_fun
-    expr = ast.parse(expr,mode='eval')
+    expr = ast.parse(expr, mode='eval')
     expr = SpeciesParser().visit(expr)
     return parsed_species
 
 
-def dependency_grapher(model,reactions):
+def dependency_grapher(model, reactions):
+    """
+    This function returns a dependency graph for a models reactions in the form of a
+    dictionary containing {species name: {'dependencies'}:[list of reaction names]}.
+    :param model: Model to used to create a reaction dependency graph
+    :param reactions: list(model.listOfReactions)
+    :return: Dependency graph dictionary
+    """
     dependent_rxns = {}
     for i in reactions:
         cust_spec = []
