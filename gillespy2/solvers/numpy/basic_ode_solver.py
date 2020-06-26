9c5ea6c48c13960a74c9786b5ad67757cc848a2b
Fin Carter <fcarter@unca.edu>
2020-06-26 08:24:22 -0400

    -TEST

--- gillespy2/solvers/numpy/basic_ode_solver.py
+++
@@ -1,7 +0,0 @@
-from .ode_solver import ODESolver
-
-
-class BasicODESolver(ODESolver):
-    from gillespy2.core import log
-    log.warning("The name 'BasicODESolver' has been deprecated, future versions of GillesPy2 will not allow"
-                " this import. Please import 'TauHybridSolver' by: from gillespy2 import ODESolver")
--- gillespy2/solvers/numpy/basic_tau_leaping_solver.py
+++
@@ -1,7 +0,0 @@
-from .tau_leaping_solver import TauLeapingSolver
-
-
-class BasicTauLeapingSolver(TauLeapingSolver):
-    from gillespy2.core import log
-    log.warning("The name 'BasicTauLeapingSolver' has been deprecated, future versions of GillesPy2 will not allow"
-                " this import. Please import 'TauLeapingSolver")
