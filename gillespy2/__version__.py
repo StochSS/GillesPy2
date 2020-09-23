0d5bb0e23b68fff19b0f611b8f234dec9f3bc17b
Sean Matthew <smatthe2@unca.edu>
2020-09-23 09:27:46 -0400

    Bump version to 1.5.5

--- gillespy2/__version__.py
+++ gillespy2/__version__.py
@@ -5,7 +5,7 @@
 # @website https://github.com/StochSS/GillesPy2
 # =============================================================================
 
-__version__      = '1.5.4'
+__version__      = '1.5.5'
 __title__        = 'GillesPy2'
 __description__  = 'Python interface for Gillespie-style biochemical simulations'
 __url__          = 'https://github.com/StochSS/GillesPy2'
