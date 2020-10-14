37e1a1f0e9d8a50311d1ba4b8b3f40baec555983
Ethan Green <egreen4@unca.edu>
2020-10-14 16:03:56 -0400

    removed previous implementation

--- gillespy2/solvers/utilities/cpp_support_test.py
+++ gillespy2/solvers/utilities/cpp_support_test.py
@@ -27,18 +27,4 @@ def check_cpp_support():
 
     return not any_missing
 
-    """
-    from gillespy2.solvers.cpp.example_models import Example
-    from gillespy2 import SSACSolver
-    try:
-        model = Example()
-        results = model.run(solver=SSACSolver, cpp_support=True)
-        return True
-    except Exception as e:
-        log.warn('Unable to use C++ optimized SSA: {0}.  The performance of ' \
-        'this package can be significantly increased if you install/configure GCC on ' \
-        'this machine.'.format(e))
-        return False
-    """
-
 cpp_support = check_cpp_support()
