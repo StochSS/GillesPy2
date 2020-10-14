8a2b56717b9631ad33a0b9461d95b7a6c373433f
Ethan Green <egreen4@unca.edu>
2020-10-14 15:06:10 -0400

    added can_use_cpp var to cpp/__init__.py to mimic can_use_numpy in numpy/__init__.py.

--- gillespy2/solvers/cpp/__init__.py
+++ gillespy2/solvers/cpp/__init__.py
@@ -2,5 +2,7 @@ from gillespy2.solvers.cpp.ssa_c_solver import SSACSolver
 from gillespy2.solvers.cpp.variable_ssa_c_solver import VariableSSACSolver
 from gillespy2.core import log
 
+from gillespy2.solvers.utilities.cpp_support_test import cpp_support
+can_use_cpp = cpp_support
 
 __all__ = ['SSACSolver', 'VariableSSACSolver']
