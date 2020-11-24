7665113db02d1d6657f3bcee473d02236997cffb
Bryan Rumsey <bmrumsey@gmail.com>
2020-11-24 10:57:24 -0500

    Fixed the dimerization __init__ function.

--- test/example_models.py
+++ test/example_models.py
@@ -73,25 +73,25 @@ class VilarOscillator(Model):
 class Dimerization(Model):
     def __init__(self, parameter_values=None):
         # First call the gillespy2.Model initializer.
-        super().__init__(self)
+        Model.__init__(self)
 
         # Define parameters for the rates of creation and dissociation.
-        k_c = gillespy2.Parameter(name='k_c', expression=0.005)
-        k_d = gillespy2.Parameter(name='k_d', expression=0.08)
+        k_c = Parameter(name='k_c', expression=0.005)
+        k_d = Parameter(name='k_d', expression=0.08)
         self.add_parameter([k_c, k_d])
 
         # Define variables for the molecular species representing M and D.
-        m = gillespy2.Species(name='monomer', initial_value=30)
-        d = gillespy2.Species(name='dimer',   initial_value=0)
+        m = Species(name='monomer', initial_value=30)
+        d = Species(name='dimer',   initial_value=0)
         self.add_species([m, d])
 
         # Define the reactions representing the process.  In GillesPy2,
         # the list of reactants and products for a Reaction object are each a
         # Python dictionary in which the dictionary keys are Species objects
         # and the values are stoichiometries of the species in the reaction.
-        r_creation     = gillespy2.Reaction(name="r_creation", rate=k_c,
+        r_creation = Reaction(name="r_creation", rate=k_c,
                                             reactants={m:2}, products={d:1})
-        r_dissociation = gillespy2.Reaction(name="r_dissociation", rate=k_d,
+        r_dissociation = Reaction(name="r_dissociation", rate=k_d,
                                             reactants={d:1}, products={m:2})
         self.add_reaction([r_creation, r_dissociation])
 
