e5e62423f309078afb28729e95050854d16fb666
mdip226 <mdip226@gmail.com>
2021-03-07 18:14:56 -0500

    copying

--- gillespy2/solvers/cpp/__init__.py
+++ gillespy2/solvers/cpp/__init__.py
@@ -1,5 +1,6 @@
 from gillespy2.solvers.cpp.ssa_c_solver import SSACSolver
 from gillespy2.solvers.cpp.ode_c_solver import ODECSolver
 from gillespy2.solvers.cpp.tau_leaping_c_solver import TauLeapingCSolver
+from gillespy2.solvers.cpp.tau_hybrid_c_solver import TauHybridCSolver
 
-__all__ = ['SSACSolver', 'ODECSolver', 'TauLeapingCSolver']
+__all__ = ['SSACSolver', 'ODECSolver', 'TauLeapingCSolver', 'TauHybridCSolver']
---
+++ gillespy2/solvers/cpp/c_base/tau_hybrid_cpp_solver/TauHybridCSolver.h
@@ -0,0 +1,9 @@
+#ifndef TAUHYBRIDCSOLVER_H
+#define TAUHYBRIDCSOLVER_H
+#include "model.h"
+
+using namespace Gillespy;
+void TauHybridSolver(Gillespy::Simulation* simulation, double increment);
+
+
+#endif 
