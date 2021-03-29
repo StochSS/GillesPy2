973e6c623c038ff34b650e3ff7e7e27f34375fed
ethangreen-dev <egreen4@unca.edu>
2021-03-29 10:07:38 -0400

    extended json encode / decode support for numpy ndarray type

--- gillespy2/core/jsonify.py
+++ gillespy2/core/jsonify.py
@@ -1,6 +1,6 @@
 from json import JSONEncoder, JSONDecoder
 
-class Jsonify:
+class Jsonify():
     def to_dict(self):
         return self.public_vars()
 
@@ -13,17 +13,26 @@ class Jsonify:
     def encode_dict(self, dict):
         return list(map(lambda x: x.to_json(), dict.values()))
 
+class StaticJsonify():
+    @staticmethod
+    def to_dict(obj):
+        pass
+
+    @staticmethod
+    def from_json(json_object):
+        pass
+
+    @staticmethod
+    def public_vars(obj):
+        return {k: v for k, v in vars(obj).items() if not k.startswith("_")}
+
 class ComplexJsonEncoder(JSONEncoder):
     def default(self, o):
         from numpy import ndarray
         from gillespy2.core.model import Model
 
         if isinstance(o, ndarray):
-            return {
-                "start": o[0],
-                "end": o[-1],
-                "points": o.size
-            }
+            return StaticNdarrayCoder.to_dict(o)
 
         if not isinstance(o, Jsonify):
             return super().default(o)
@@ -37,7 +46,7 @@ class ComplexJsonEncoder(JSONEncoder):
 
         return model
 
-class ComplexTypeDecoder():
+class ComplexJsonDecoder():
     @staticmethod
     def decode_hook(obj):
         from pydoc import locate
@@ -50,7 +59,22 @@ class ComplexTypeDecoder():
         if obj_type is None:
             raise Exception(f"{obj_type} does not exist.")
 
-        if not issubclass(obj_type, Jsonify):
+        if not issubclass(obj_type, Jsonify) and not issubclass(obj_type, StaticJsonify):
             raise Exception(f"{obj_type}")
 
         return obj_type.from_json(obj)
+
+class StaticNdarrayCoder(StaticJsonify):
+    @staticmethod
+    def to_dict(obj):
+        return {
+            "start": obj[0],
+            "end": obj[-1],
+            "size": obj.size,
+            "_type": f"{StaticNdarrayCoder.__module__}.{StaticNdarrayCoder.__name__}"
+        }
+
+    @staticmethod
+    def from_json(json_object):
+        import numpy
+        return numpy.linspace(start=json_object["start"], stop=json_object["end"], num=json_object["size"])
