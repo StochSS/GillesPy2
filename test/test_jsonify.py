2c3e2d84ab0872f6983a326e3d264100adcfcc53
ethangreen-dev <egreen4@unca.edu>
2021-04-01 02:03:35 -0400

    expanded out test cases to cover model -> json and json -> model assertions for each example model

--- test/test_jsonify.py
+++ test/test_jsonify.py
@@ -4,9 +4,41 @@ sys.path.append("..")
 from example_models import *
 
 class TestJsonModels(unittest.TestCase):
-    def __init__(self):
-        import inspect
+    def test_jsonify_all_models(self):
+        models = [
+            Example,
+            MichaelisMenten,
+            VilarOscillator, 
+            Dimerization, 
+            Trichloroethylene, 
+            LacOperon, 
+            Schlogl, 
+            ToggleSwitch,
+            Tyson2StateOscillator,
+            Oregonator,
+        ]
 
-        models = list((x for x in inspect.importlib.import_module("example_models") if x.isclass))
         for model in models:
-            self.test_model(model)
+            self._test_model(model)
+
+
+    def _test_model(self, model):
+        print(f"Running: {model.__name__}")
+
+        target = model()
+        non_anon_json = target.to_json()
+        non_anon_from_json = model.from_json(non_anon_json)
+        non_anon_back_into_json = non_anon_from_json.to_json()
+
+        self.assertEqual(non_anon_json, non_anon_back_into_json)
+        print("\t[PASS] Non-anonymous json conversion.")
+
+        target2 = model()
+        table = target2.get_translation_table()
+
+        anon_json = target2.to_json(table)
+        anon_from_json = model.from_json(anon_json)
+        anon_back_into_json = anon_from_json.to_json(table)
+
+        self.assertEqual(anon_json, anon_back_into_json)
+        print("\t[PASS] Anonymous json conversion.")
