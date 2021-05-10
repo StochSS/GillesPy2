1cebaa0dddd627c89312ff81b39919c16098cd4b
Joshua Cooper <42880781+jtcooper10@users.noreply.github.com>
2021-05-10 18:45:24 -0400

    Fix missing import in __init__.py
    `VariableSSACSolver` import mysteriously went missing during the merge from the `develop` branch; re-added

--- gillespy2/solvers/cpp/__init__.py
+++ gillespy2/solvers/cpp/__init__.py
@@ -1,6 +1,7 @@
 from gillespy2.solvers.cpp.ssa_c_solver import SSACSolver
 from gillespy2.solvers.cpp.ode_c_solver import ODECSolver
 from gillespy2.solvers.cpp.tau_leaping_c_solver import TauLeapingCSolver
+from gillespy2.solvers.cpp.variable_ssa_c_solver import VariableSSACSolver
 from gillespy2.solvers.cpp.tau_hybrid_c_solver import TauHybridCSolver
 
 # Call external function instead of implementing here so we don't need to rerun check on each model init.
