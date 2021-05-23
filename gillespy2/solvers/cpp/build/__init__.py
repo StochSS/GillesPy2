694bc2bfa56359bee1a4287b1e750990af253a52
Ethan Green <egreen4@unca.edu>
2021-05-23 01:19:41 -0400

    Moved from os.path to pathlib.Path

--- gillespy2/solvers/cpp/build/__init__.py
+++ gillespy2/solvers/cpp/build/__init__.py
@@ -1,23 +1,27 @@
-import os, tempfile, shutil
+import tempfile, shutil
+from pathlib import Path
 
-from . import make, template_gen
+from . import template_gen
+from make import Make
 from gillespy2.core import Model
 
 class BuildEngine():
     template_definitions_name = "template_definitions.h"
 
     def __init__(self):
-        self.self_dir = os.path.dirname(os.path.abspath(__file__))
-        self.cpp_dir = os.path.join(self.self_dir, "../c_base/")
+        self.self_dir = Path(__file__).parent
+        self.cpp_dir = self.self_dir.joinpath("../c_base").resolve()
+        self.temp_dir = Path(tempfile.gettempdir())
+        self.makefile = self.cpp_dir.joinpath("Makefile")
+
+        self.make = Make(self.makefile, self.temp_dir)
 
     def __enter__(self):
         """
         Create a new temp directory.
         """
 
-        self.temp_dir = tempfile.gettempdir()
-        os.mkdir(self.temp_dir)
-
+        self.temp_dir.mkdir()
         return self
 
     def __exit__(self):
@@ -32,8 +36,12 @@ class BuildEngine():
         Copy everything but the template definitions header to the temp directory.
         """
 
+        # If object files haven't been compiled yet, go ahead and compile them with make.
+        if not self.make.obj_dir.is_dir():
+            self.make.prebuild()
+
         shutil.copytree(self.cpp_dir, self.temp_dir)
-        os.remove(os.path.join(self.temp_dir, self.template_definitions_name))
+        self.temp_dir.joinpath(self.template_definitions_name).unlink()
 
     def write_template(self, model: Model, variable=False):
         """
@@ -46,4 +54,12 @@ class BuildEngine():
         :type variable: bool
         """
 
-        template_gen.write_template(os.path.join(self.temp_dir, self.template_definitions_name), model, variable)
+        template_gen.write_template(self.temp_dir.joinpath(self.template_definitions_name), model, variable)
+
+    def build_solver(self, solver_name: str):
+        """
+        Build the solver to the temp directory.
+
+        :param solver_name: The name of the solver to build. For example, ODESolver.
+        :type solver_name: str
+        """
