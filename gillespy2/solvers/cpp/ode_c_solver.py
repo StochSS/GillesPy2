9ca5eab7d613f10a20314c7a15c6bdedb9fc53c3
Ethan Green <egreen4@unca.edu>
2021-05-24 18:43:09 -0400

    Implemented run() with CSimulation functions.

--- gillespy2/solvers/cpp/ode_c_solver.py
+++ gillespy2/solvers/cpp/ode_c_solver.py
@@ -1,6 +1,7 @@
-from gillespy2.core import GillesPySolver, Model
-from gillespy2.solvers.cpp.c_simulation import CSimulation
-from gillespy2.solvers.cpp.build import BuildEngine
+from gillespy2.solvers.cpp.c_decoder import BasicSimDecoder, SimDecoder
+from gillespy2.core import GillesPySolver
+from .c_simulation import CSimulation, SimulationReturnCode
+from ..utilities import cutils
 
 class ODECSolver(GillesPySolver, CSimulation):
     type = "ODESimulation"
@@ -14,6 +15,31 @@ class ODECSolver(GillesPySolver, CSimulation):
     def run(self=None, model=None, t=20, number_of_trajectories=1, timeout=0,
             increment=0.05, seed=None, debug=False, profile=False, variables={}, resume=None, **kwargs):
 
-        pause = False
+        t = abs(t - int(resume["time"][-1]))
+        number_timesteps = int(round(t / increment + 1))
+        seed = int(seed)
 
-        # ... After some checks and setup, execute the simulation.
+        args = {
+            "trajectories": number_of_trajectories,
+            "timesteps": number_timesteps,
+            "end": t,
+            "increment": increment,
+            "seed": seed
+        }
+
+        if self.variable:
+            populations = cutils.update_species_init_values(model.listOfSpecies, self.species, variables, resume)
+            parameter_values = cutils.change_param_values(model.listOfParameters, self.parameters, model.volume, variables)
+
+            args.update({
+                "initial_values": populations,
+                "parameters": parameter_values
+            })
+
+        args = self._make_args(args)
+        decoder = BasicSimDecoder()
+
+        sim_status = self._run(self.type, args, decoder, timeout)
+
+        if sim_status == SimulationReturnCode.DONE:
+            return decoder.get_output(), 0
