2ecc42da113baa83c79bbff529c3fd067fc6c289
Ethan Green <egreen4@unca.edu>
2021-05-28 14:11:15 -0400

    Updated logging message if there are missing dependencies.

--- gillespy2/solvers/cpp/__init__.py
+++ gillespy2/solvers/cpp/__init__.py
@@ -1,4 +1,3 @@
-from gillespy2.solvers.cpp import build
 from gillespy2.core import logging
 from gillespy2.solvers.cpp.ssa_c_solver import SSACSolver
 from gillespy2.solvers.cpp.ode_c_solver import ODECSolver
@@ -11,7 +10,7 @@ missing_deps = BuildEngine.get_missing_dependencies()
 
 if len(missing_deps) > 0:
     logging.warn(
-        f"Unable to use C++ optimized SSA due to one or more missing dependencies: {missing_deps}. "
+        f"Unable to use C++ optimized solvers due to one or more missing dependencies: {missing_deps}. "
         "The performance of this package can be significantly increased if you install/configure "
         "these on your machine."
     )
