9a3ec65701908617ff511544432d31b4e8c9e91b
Ethan Green <egreen4@unca.edu>
2021-05-28 13:16:28 -0400

    Deprecated old C++ dependency check. - `Removed cpp_support_test.py` - Remove reference to `cpp_support_test.py` - Modified `test_check_cpp_support.py` to use `BuildEngine`.

--- gillespy2/solvers/cpp/__init__.py
+++ gillespy2/solvers/cpp/__init__.py
@@ -1,11 +1,20 @@
+from gillespy2.solvers.cpp import build
+from gillespy2.core import logging
 from gillespy2.solvers.cpp.ssa_c_solver import SSACSolver
 from gillespy2.solvers.cpp.ode_c_solver import ODECSolver
 from gillespy2.solvers.cpp.tau_leaping_c_solver import TauLeapingCSolver
 from gillespy2.solvers.cpp.variable_ssa_c_solver import VariableSSACSolver
 
-# Call external function instead of implementing here so we don't need to rerun check on each model init.
-from gillespy2.solvers.utilities.cpp_support_test import cpp_support
-can_use_cpp = cpp_support
+# Check to see if we're missing any dependencies.
+from .build.build_engine import BuildEngine
+missing_deps = BuildEngine.get_missing_dependencies()
+
+if len(missing_deps) > 0:
+    logging.warn(
+        f"Unable to use C++ optimized SSA due to one or more missing dependencies: {missing_deps}. "
+        "The performance of this package can be significantly increased if you install/configure "
+        "these on your machine."
+    )
 
 __all__ = ['SSACSolver', 'VariableSSACSolver']
 __all__ = ['SSACSolver', 'ODECSolver', 'TauLeapingCSolver', 'VariableSSACSolver']
