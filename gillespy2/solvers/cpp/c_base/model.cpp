54964829c11601613e292979107ec40fea111915
Josh C <jtcooper90@gmail.com>
2021-06-04 11:20:57 -0400

    Templated version of Simulation class
    - Define data type of trajectories as template type
    - Explicitly instantiate templated classes for continuous (double) and discrete (unsigned int)

--- gillespy2/solvers/cpp/c_base/model.cpp
+++ gillespy2/solvers/cpp/c_base/model.cpp
@@ -2,6 +2,9 @@
 
 namespace Gillespy{
 
+    template void init_simulation<double>(Model *model, Simulation<double> &simulation);
+    template void init_simulation<unsigned int>(Model *model, Simulation<unsigned int> &simulation);
+
   Model :: Model(std :: vector<std :: string> species_names, std :: vector<unsigned int> species_populations, std :: vector<std :: string> reaction_names):
     number_species(species_names.size()),
     number_reactions(reaction_names.size())
@@ -40,89 +43,75 @@ namespace Gillespy{
     }
   }
 
-  void simulationSSAINIT(Model* model, Simulation &simulation){
-  	simulation.timeline = new double[simulation.number_timesteps];
-    double timestep_size = simulation.end_time/(simulation.number_timesteps-1);
-    for(unsigned int i = 0; i < simulation.number_timesteps; i++){
-    	simulation.timeline[i] = timestep_size * i;
-    }
- 	unsigned int trajectory_size = simulation.number_timesteps * (model -> number_species);
-    simulation.trajectories_1D = new unsigned int[simulation.number_trajectories * trajectory_size];
-    simulation.trajectories = new unsigned int**[simulation.number_trajectories];
-    for(unsigned int i = 0; i < simulation.number_trajectories; i++){
-    	simulation.trajectories[i] = new unsigned int*[simulation.number_timesteps];
-    	for(unsigned int j = 0; j < simulation.number_timesteps; j++){
-    		simulation.trajectories[i][j] = &(simulation.trajectories_1D[i * trajectory_size + j *  (model -> number_species)]);
-      }
-    }
-  }
+	template <typename TNum>
+	void init_timeline(Model *model, Simulation<TNum> &simulation)
+	{
+		double timestep_size = simulation.end_time / (simulation.number_timesteps - 1);
+		simulation.timeline = new double[simulation.number_timesteps];
 
-
-void simulationODEINIT(Model* model, Simulation &simulation){
-    simulation.timeline = new double[simulation.number_timesteps];
-    double timestep_size = simulation.end_time/(simulation.number_timesteps-1);
-    for(unsigned int i = 0; i < simulation.number_timesteps; i++){
-      simulation.timeline[i] = timestep_size * i;
-    }
-
-
-    unsigned int trajectory_size = simulation.number_timesteps * (model -> number_species);
-    simulation.trajectories_1DODE = new double[simulation.number_trajectories * trajectory_size];
-    simulation.trajectoriesODE = new double**[simulation.number_trajectories];
-    for(unsigned int i = 0; i < simulation.number_trajectories; i++){
-      simulation.trajectoriesODE[i] = new double*[simulation.number_timesteps];
-      for(unsigned int j = 0; j < simulation.number_timesteps; j++){
-	simulation.trajectoriesODE[i][j] = &(simulation.trajectories_1DODE[i * trajectory_size + j *  (model -> number_species)]);
-      }
-    }
-  }
-
-
-  Simulation :: ~Simulation(){
-    delete timeline;
-    if (ISODE==1){
-    delete trajectories_1DODE;
-    for(unsigned int i = 0; i < number_trajectories; i++){
-      delete trajectoriesODE[i];
-    }
-    delete trajectoriesODE;
-    }else{
-
-    delete trajectories_1D;
-     for(unsigned int i = 0; i < number_trajectories; i++){
-      delete trajectories[i];
-    }
-    delete trajectories;
-    }
-  }
-
-
-  std :: ostream& operator<<(std :: ostream& os, const Simulation& simulation){
-    for(unsigned int i = 0; i < simulation.number_timesteps; i++){
-      os << simulation.timeline[i] << " ";
-      for(unsigned int trajectory = 0; trajectory < simulation.number_trajectories; trajectory++){
-	for(unsigned int j = 0; j < simulation.model -> number_species; j++){
-	    if (simulation.ISODE==1){os << simulation.trajectoriesODE[trajectory][i][j] <<  " ";}
-	    else{os << simulation.trajectories[trajectory][i][j] <<  " ";}
+		for (unsigned int i = 0; i < simulation.number_timesteps; ++i) {
+			simulation.timeline[i] = timestep_size * i;
+		}
 	}
-      }
-      os << "\n";
-    }
-    return os;
-  }
 
-void Simulation :: output_results_buffer(std::ostream& os){
-    for (int i = 0 ; i < number_trajectories; i++){
-        for (int j = 0; j<number_timesteps;j++){
-            os<<timeline[j]<<',';
-            for (int k = 0; k<model->number_species; k++){
-                if (ISODE==1){os<<trajectoriesODE[i][j][k]<<',';}
-                else{os<<trajectories[i][j][k]<<',';}
-                }
-            }
-         }
-    os<<(int)current_time;
-    }
+	template <typename TNum>
+	void init_simulation(Model *model, Simulation<TNum> &simulation)
+	{
+		init_timeline(model, simulation);
 
+		unsigned int trajectory_size = simulation.number_timesteps * (model -> number_species);
+		simulation.trajectories_1D = new TNum[simulation.number_trajectories * trajectory_size];
+		simulation.trajectories = new TNum**[simulation.number_trajectories];
 
+		for(unsigned int i = 0; i < simulation.number_trajectories; i++){
+			simulation.trajectories[i] = new TNum*[simulation.number_timesteps];
+			for(unsigned int j = 0; j < simulation.number_timesteps; j++){
+				simulation.trajectories[i][j] = &(simulation.trajectories_1D[i * trajectory_size + j *  (model -> number_species)]);
+			}
+		}
+	}
+
+	template <typename TNum>
+	Simulation<TNum>::~Simulation(){
+		delete timeline;
+		delete trajectories_1D;
+
+		for (unsigned int i = 0; i < number_trajectories; i++) {
+			delete trajectories[i];
+		}
+
+		delete trajectories;
+	}
+
+	template <typename TNum>
+	std :: ostream& operator<<(std :: ostream& os, const Simulation<TNum> &simulation){
+		for (unsigned int i = 0; i < simulation.number_timesteps; i++) {
+			os << simulation.timeline[i] << ' ';
+
+			for(unsigned int trajectory = 0; trajectory < simulation.number_trajectories; trajectory++){
+				for(unsigned int j = 0; j < simulation.model -> number_species; j++){
+					os << simulation.trajectories[trajectory][i][j] << ' ';
+				}
+			}
+
+			os << simulation.timeline[i] << ' ';
+			os << '\n';
+		}
+
+		return os;
+	}
+
+	template <typename TNum>
+	void Simulation<TNum> :: output_results_buffer(std::ostream& os){
+		for (int i = 0 ; i < number_trajectories; i++){
+			for (int j = 0; j < number_timesteps; j++){
+				os << timeline[j] << ',';
+				for (int k = 0; k < model->number_species; k++){
+					os << trajectories[i][j][k] << ',';
+				}
+			}
+		}
+
+		os<<(int)current_time;
+	}
 }
--- gillespy2/solvers/cpp/c_base/model.h
+++ gillespy2/solvers/cpp/c_base/model.h
@@ -8,69 +8,74 @@
 
 namespace Gillespy{
 
-  //Represents info for a chemical reactant/product
-  struct Species{
-    unsigned int id; //useful for index id in arrays
-    std :: string name;
-    unsigned int initial_population;
-    //Used for hashing into set, for TauLeapingCSolver
-    bool operator < (const Species &other) const { return id < other.id; }
-  };
+	//Represents info for a chemical reactant/product
+	struct Species{
+		unsigned int id; //useful for index id in arrays
+		std :: string name;
+		unsigned int initial_population;
+		//Used for hashing into set, for TauLeapingCSolver
+		bool operator < (const Species &other) const { return id < other.id; }
+	};
 
-  struct Reaction{
-    unsigned int id; //useful for propensity function id associated
-    std :: string name;
-    std :: unique_ptr<int[]> species_change; //list of changes to species with this reaction firing
-    std :: vector<unsigned int> affected_reactions; //list of which reactions have propensities that would change with this reaction firing
-  };
+	struct Reaction{
+		unsigned int id; //useful for propensity function id associated
+		std :: string name;
+		std :: unique_ptr<int[]> species_change; //list of changes to species with this reaction firing
+		std :: vector<unsigned int> affected_reactions; //list of which reactions have propensities that would change with this reaction firing
+	};
   
-  //Represents a model of reactions and species
-  struct Model{
-      void update_affected_reactions();
-    unsigned int number_species;
-    std :: unique_ptr<Species[]> species;
-    unsigned int number_reactions;
-    std :: unique_ptr<Reaction[]> reactions;
-    Model(std :: vector<std :: string> species_names, std :: vector<unsigned int> species_populations, std :: vector<std :: string> reaction_names);
-  };
+	//Represents a model of reactions and species
+	struct Model{
+		void update_affected_reactions();
+		unsigned int number_species;
+		std :: unique_ptr<Species[]> species;
+		unsigned int number_reactions;
+		std :: unique_ptr<Reaction[]> reactions;
+		Model(std :: vector<std :: string> species_names, std :: vector<unsigned int> species_populations, std :: vector<std :: string> reaction_names);
+	};
   
-  //Interface class to represent container for propensity functions
-  class IPropensityFunction{
-  public:
-    virtual double evaluate(unsigned int reaction_number, unsigned int* state) = 0;
-    virtual double TauEvaluate(unsigned int reaction_number, const std::vector<int> &S) = 0;
-    virtual double ODEEvaluate(int reaction_number, const std::vector <double> &S) = 0;
-    virtual ~IPropensityFunction() {};
-  };
+	//Interface class to represent container for propensity functions
+	class IPropensityFunction{
+	public:
+		virtual double evaluate(unsigned int reaction_number, unsigned int* state) = 0;
+		virtual double TauEvaluate(unsigned int reaction_number, const std::vector<int> &S) = 0;
+		virtual double ODEEvaluate(int reaction_number, const std::vector <double> &S) = 0;
+		virtual ~IPropensityFunction() {};
+	};
 
+	template <typename PType>
+	struct Simulation{
+		Model* model;
+		~Simulation();
 
+		double* timeline;
+		double end_time;
+		double current_time;
+		int random_seed;
 
-  struct Simulation{
-    Model* model;
-    ~Simulation();
+		unsigned int number_timesteps;
+		unsigned int number_trajectories;
 
-    int ISODE = 0; // if 0, not ODE sim, if 1, ODE sim
-    double* timeline;
-    double end_time;
-    double current_time;
-    int random_seed;
+		PType *trajectories_1D;
+		PType ***trajectories;
 
-    unsigned int number_timesteps;
-    unsigned int number_trajectories;
+		IPropensityFunction *propensity_function;
+		friend std :: ostream& operator<<(std :: ostream& os, const Simulation<PType> &simulation);
+		void output_results_buffer(std :: ostream& os);
+	};
 
-    unsigned int* trajectories_1D;
-    unsigned int*** trajectories;
+	/**
+	 * Trajectory initializer function
+	 * Populates the simulation object's data based on the given model
+	 */
+	template <typename TNum>
+	void init_simulation(Model *model, Simulation<TNum> &simulation);
 
-    double* trajectories_1DODE;
-    double*** trajectoriesODE;
-
-    IPropensityFunction *propensity_function;
-    friend std :: ostream& operator<<(std :: ostream& os, const Simulation& simulation);
-    void output_results_buffer(std :: ostream& os);
-  };
-  //Trajectory initializers for ODE and SSA solvers
-  void simulationODEINIT(Model* model, Simulation &simulation);
-  void simulationSSAINIT(Model* model, Simulation &simulation);
+	// Explicitly instantiate each needed type of simulation
+	template struct Simulation<double>;
+	template struct Simulation<unsigned int>;
+	extern template void init_simulation<double>(Model *model, Simulation<double> &simulation);
+	extern template void init_simulation<unsigned int>(Model *model, Simulation<unsigned int> &simulation);
 
 }
 #endif
