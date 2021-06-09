7ff2d00cba045313f4fb12024803ec841d7fdee4
ethangreen-dev <egreen4@unca.edu>
2021-06-09 12:45:05 -0400

    Implemented Python profiler.  - Added cProfile support.  - Added pstats Stats object parsing.  - Added PerformanceData output.

--- test/perf/python_profiler.py
+++ test/perf/python_profiler.py
@@ -1,5 +1,10 @@
-import pstats
 import cProfile
+
+import pstats
+from pstats import SortKey
+
+from . import PerformanceData
+from . import PerformanceEntry
 
 from gillespy2.core import Model
 from gillespy2.core import GillesPySolver
@@ -20,6 +25,20 @@ def run_profiler(model: Model, solver: GillesPySolver, trajectories=4, timesteps
     solver.run(model=model, number_of_trajectories=trajectories, t=timesteps)
 
     profiler.disable()
+    stats = pstats.Stats(profiler).strip_dirs().sort_stats(SortKey.TIME)
 
-    stats = pstats.Stats(profiler).sort_stats("ncalls")
-    stats.print_stats()
+    perf_data = PerformanceData()
+    perf_data.perf_time = stats.total_tt
+    perf_data.call_time = stats.total_tt
+    
+    worst_func = stats.fcn_list[0]
+    (cc, nc, tt, ct, callers) = stats.stats[worst_func]
+    perf_data.worst_entry = (worst_func, PerformanceEntry(round(tt, 3), round((tt / stats.total_tt) * 100)))
+
+    for func in stats.fcn_list:
+        (cc, nc, tt, ct, callers) = stats.stats[func]
+
+        perf_entry = PerformanceEntry(t=round(tt, 3), percent=round((tt / stats.total_tt) * 100, 3))
+
+        parent = func[0] if func[0] != "~" else "python"
+        perf_data.call_list[f"{parent}:{func[2]}"] = perf_entry
