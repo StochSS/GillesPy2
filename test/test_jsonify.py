236e8f0faee0c2b122c361827b719ba9fd2b2148
ethangreen-dev <egreen4@unca.edu>
2021-06-10 16:25:37 -0400

    Updated `Jsonify` tests.  + Added documentation to clarify the intent and behavior of the tests.  + General cleanup, added formatting and organized imports.

--- test/test_jsonify.py
+++ test/test_jsonify.py
@@ -1,4 +1,5 @@
-import sys, unittest
+import sys
+import unittest
 
 sys.path.append("..")
 from example_models import *
@@ -25,31 +26,50 @@ class TestJsonModels(unittest.TestCase):
     ]
 
     def test_non_anon_model_norun(self):
-        # Convert a model to json, back again, and then back into json. It should be equal to the original.
+        """
+        Test that a model can be converted to JSON and back.
+        """
+
         for model in self.models:
             target = model()
 
-            self.assertEqual(target.to_json(), model.from_json(target.to_json()).to_json())
+            self.assertEqual(
+                target, 
+                model.from_json(target.to_json())
+            )
 
     def test_anon_model_norun(self):
+        """
+        Test that an anonymous model can be converted to JSON and back.
+        """
+
         for model in self.models:
             target = model()
-            anon_json = target.to_anon().to_json()
+            anon_target = target.to_anon()
 
-            self.assertEqual(anon_json, model.from_json(anon_json).to_json())
+            self.assertEqual(
+                anon_target, 
+                model.from_json(anon_target.to_json())
+            )
 
     def test_non_anon_model_run(self):
+        """
+        Test that Model simulation results can be converted to JSON and back.
+        """
+
         for model in self.runnable_models:
             target = model()
             results = target.run()
 
-            r_json = results.to_json()
-            r_from = Results.from_json(r_json)
-            r_back = r_from.to_json()
+            results_from = Results.from_json(results.to_json())
 
-            self.assertEqual(r_json, r_back)
+            self.assertEquals(results.to_json(), results_from.to_json())
 
     def test_anon_model_runs(self):
+        """
+        Test that anonymous Model simulation results can be converted to JSON and back.
+        """
+
         for model in self.runnable_models:
             target = model()
 
@@ -60,12 +80,19 @@ class TestJsonModels(unittest.TestCase):
             self.assertEqual(results, Results.from_json(results).to_json())
 
     def test_model_hash(self):
+        """
+        Test the accuracy of the JSON hash.
+        """
+
         for model in self.models:
             # Simple test to see if two identical models will return the same hash.
             model_1 = model()
             model_2 = model()
 
-            self.assertEqual(model_1.to_anon().get_json_hash(), model_2.to_anon().get_json_hash())
+            self.assertEqual(
+                model_1.to_anon().get_json_hash(), 
+                model_2.to_anon().get_json_hash()
+            )
 
             # Create a test class and change the variable insertion order.
             model_1 = model()
@@ -80,15 +107,34 @@ class TestJsonModels(unittest.TestCase):
 
             translation_table = model_1.get_translation_table()
 
-            # A bit overkill, but it's good to check to ensure that both the JSON and hash output match.
-            self.assertEqual(model_1.to_anon().to_json(), model_2.to_anon().to_json())
-            self.assertEqual(model_1.to_anon().get_json_hash(), model_2.to_anon().get_json_hash())
+            # Test to ensure that the JSON output and JSON hash are equal.
+            self.assertEqual(
+                model_1.to_anon().to_json(), 
+                model_2.to_anon().to_json()
+            )
 
-            # The translation for model_1 and model_2 should be the same.
-            self.assertEqual(model_1.get_translation_table().to_json(), model_2.get_translation_table().to_json())
+            self.assertEqual(
+                model_1.to_anon().get_json_hash(),
+                model_2.to_anon().get_json_hash()
+            )
 
-            self.assertEqual(model_1.to_json(), Model.from_json(model_2.to_anon().to_json()).to_named().to_json())
-            self.assertEqual(model_1.to_anon().get_json_hash(), Model.from_json(model_2.to_anon().to_json()).get_json_hash())
+            # Test to ensure that the translation table for both models is identical.
+            self.assertEqual(
+                model_1.get_translation_table().to_json(), 
+                model_2.get_translation_table().to_json()
+            )
+
+            # Test to ensure that model_1's JSON is equivalent to model_2 -> anon -> json -> object -> named -> json.
+            self.assertEqual(
+                model_1.to_json(), 
+                Model.from_json(model_2.to_anon().to_json()).to_named().to_json()
+            )
+
+            # Test to ensure that model_2's anon JSON hash is equivalent to model_2 -> anon -> json -> object -> json hash.
+            self.assertEqual(
+                model_1.to_anon().get_json_hash(), 
+                Model.from_json(model_2.to_anon().to_json()).get_json_hash()
+            )
 
     def test_model_hash_chaos(self):
         import random
@@ -96,7 +142,6 @@ class TestJsonModels(unittest.TestCase):
         for model in self.models:
             model_1 = model()
             model_2 = model()
-
 
             # Generate lists of random parameters, species, and reactions. Each will then be added to model_1 and model_2 in random order.
             parameters = [Parameter(name=bytes(random.sample(range(97, 123), 10)).decode(), expression=random.randint(0, 10)) for x in range(5)]
