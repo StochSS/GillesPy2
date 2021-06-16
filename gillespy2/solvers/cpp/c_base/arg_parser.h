4cd432f6eec817d0bb353c8d722b132b5ebe07b9
Matthew Dippel <mdip226@gmail.com>
2021-06-16 15:24:18 -0400

    handle multiple arguments

--- gillespy2/solvers/cpp/c_base/arg_parser.h
+++ gillespy2/solvers/cpp/c_base/arg_parser.h
@@ -2,14 +2,13 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <getopt.h>
+// #include <getopt.h>
 #include <string.h>
+#include <stdlib.h>
 
 class ArgParser
 {
 private:
-    
-
     char match_arg(char* opt)
     {
         if (!strcmp(opt, "timesteps"))
@@ -29,8 +28,7 @@ private:
         if (!strcmp(opt, "trajectories"))
             return 'T';
         if (!strcmp(opt, "tau_tol"))
-            return 0;
-        
+            return 0;      
     };
 public:
     int trajectories;
@@ -67,13 +65,9 @@ public:
     ~ArgParser();
 };
 
-ArgParser::ArgParser(int argc, char* argv[]):
-    
+ArgParser::ArgParser(int argc, char* argv[]):    
 {
-    // long_options 
-    // args = std::map<char*, int>();
     int opt;
-    // options arg;
     for (int i = 1; i < argc; ++i){
         if (argv[i][0] == '-' && argv[i][1] != '-') {
             opt = argv[i][1];
@@ -86,77 +80,38 @@ ArgParser::ArgParser(int argc, char* argv[]):
                 timesteps = atoi(argv[i+1]);
                 break;
             case 'e':
-                end = atoi(argv[i+1]);
+                end = strtod(argv[i+1],0);
                 break;
             case 's':
-                args["end"] = atoi(argv[i+1]);
+                seed = atoi(argv[i+1]);
                 break;
             case 'S':
+                switch_tol = strtod(argv[i+1],0);
                 break;
             case 'i':
+                increment = strtod(argv[i + 1], 0);
                 break;
             case 'I':
+                for (int j = i+1; argv[j][0]!= '-'; ++j){
+                    init_pop.push_back(atoi(argv[j]));
+                }
                 break;
             case 'p':
+                for (int j = i + 1; argv[j][0] != '-'; ++j)
+                {
+                    parameters.push_back(strtod(argv[j], 0));
+                }
                 break;
             case 'T':
+                trajectories = atoi(argv[i+1]);
                 break;
             case 0:
+                tau_tol = strtod(argv[i + 1], 0);
                 break;
             default:
-                break;        }
+                break;
+        }        
     }
-    // while (1) {
-    //     static struct option long_options[] = {
-    //         {"timesteps", required_argument, 0, 't'},
-    //         {"end", required_argument, 0, 'e'},
-    //         {"seed", required_argument, 0, 's'},
-    //         {"trajectories", required_argument, 0, 'T'},
-    //         {"increment", required_argument, 0, 'i'},
-    //         {"tau_tol", required_argument, 0, 0},
-    //         {"switch_tol", required_argument, 0, 'S'},
-    //         {"initpop", required_argument, 0, 'I'},
-    //         {"parameters", required_argument, 0, 'P'},
-    //         {0,0,0,0}
-    //     };
-    //     opt = getopt_long(argc, argv, "t:e:s:T:i:0:S:I:P:",long_options, NULL);
-    //     // std::cout << opt;
-    //     if (opt == -1)
-    //         break;
-        
-    //     switch (opt)
-    //     {
-    //     case 0:
-    //         args.insert(std::pair<char *, int>("tau_tol", atoi(optarg)));
-    //         break;
-    //     case 't':
-    //         args.insert(std::pair<char *, int>("timesteps", atoi(optarg)));
-    //         break;
-    //     case 'e':
-    //         args.insert(std::pair<char *, int>("end", atoi(optarg)));
-    //         break;
-    //     case 's':
-    //         args.insert(std::pair<char *, int>("seed", atoi(optarg)));
-    //         break;
-    //     case 'T':
-    //         args.insert(std::pair<char *, int>("trajectories", atoi(optarg)));
-    //         break;
-    //     case 'i':
-    //         args.insert(std::pair<char *, int>("increment", atoi(optarg)));
-    //         break;
-    //     case 'S':
-    //         break;
-    //     case 'I':
-    //         // args.insert(std::pair<char *, int>("initpop", atoi(optarg)));
-    //         break;
-    //     case 'P':
-    //         // args.insert(std::pair<char *, int>("parameters", atoi(optarg)));
-    //         break;
-        
-    //     default:
-    //         break;
-    //     }
-    // }
 
 }
 
