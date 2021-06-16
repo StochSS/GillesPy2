b9fd0765eb201826986561da0a042a48553688be
Matthew Dippel <mdip226@gmail.com>
2021-06-16 14:59:54 -0400

    simplify

--- gillespy2/solvers/cpp/c_base/arg_parser.h
+++ gillespy2/solvers/cpp/c_base/arg_parser.h
@@ -3,37 +3,108 @@
 #include <vector>
 #include <iostream>
 #include <getopt.h>
+#include <string.h>
 
 class ArgParser
 {
 private:
-    std::map<char*, int> args; 
-    std::map<char*, char*> parameters;
-    std::vector<int> init_pop;
-    void parse_short(int opt_i, char* argv[]){
+    
 
+    char match_arg(char* opt)
+    {
+        if (!strcmp(opt, "timesteps"))
+            return 't';
+        if (!strcmp(opt, "end"))
+            return 'e';
+        if (!strcmp(opt, "seed"))
+            return 's';
+        if (!strcmp(opt, "switch_tol"))
+            return 'S';
+        if (!strcmp(opt, "increment")) //double
+            return 'i';
+        if (!strcmp(opt, "init_pop"))
+            return 'I';
+        if (!strcmp(opt, "parameters"))
+            return 'p';
+        if (!strcmp(opt, "trajectories"))
+            return 'T';
+        if (!strcmp(opt, "tau_tol"))
+            return 0;
+        
     };
-    void parse_long(int opt_i, char* argv[]);
-    // int init_pop[];
 public:
-    int get(char* arg) {
-        return args[arg];
-    };
+    int trajectories;
+    int timesteps;
+    double end;
+    int seed;
+    double increment;
+    double switch_tol;
+    double tau_tol;
+    std::vector<int> init_pop;
+    std::vector<double> parameters;
+    // int get(char* arg) {
+    //     if (!strcmp(arg, "timesteps"))
+    //         return 't';
+    //     if (!strcmp(arg, "end"))
+    //         return 'e';
+    //     if (!strcmp(arg, "seed"))
+    //         return 's';
+    //     if (!strcmp(arg, "switch_tol"))
+    //         return 'S';
+    //     if (!strcmp(arg, "increment")) //double
+    //         return 'i';
+    //     if (!strcmp(arg, "init_pop"))
+    //         return 'I';
+    //     if (!strcmp(arg, "parameters"))
+    //         return 'p';
+    //     if (!strcmp(arg, "trajectories"))
+    //         return 'T';
+    //     if (!strcmp(arg, "tau_tol"))
+    //         return 0;
+    //     return args[arg];
+    // };
     ArgParser(const int argc, char *argv[]);
     ~ArgParser();
 };
 
-ArgParser::ArgParser(int argc, char* argv[])
+ArgParser::ArgParser(int argc, char* argv[]):
+    
 {
-    args = std::map<char*, int>();
+    // long_options 
+    // args = std::map<char*, int>();
     int opt;
+    // options arg;
     for (int i = 1; i < argc; ++i){
         if (argv[i][0] == '-' && argv[i][1] != '-') {
-            parse_short(i, argv);
+            opt = argv[i][1];
         }
         if (argv[i][0] == '-' && argv[i][1] == '-') {
-            parse_long(i, argv);
+            opt = match_arg((char*)argv[i][2]);
         }
+        switch(opt) {
+            case 't':
+                timesteps = atoi(argv[i+1]);
+                break;
+            case 'e':
+                end = atoi(argv[i+1]);
+                break;
+            case 's':
+                args["end"] = atoi(argv[i+1]);
+                break;
+            case 'S':
+                break;
+            case 'i':
+                break;
+            case 'I':
+                break;
+            case 'p':
+                break;
+            case 'T':
+                break;
+            case 0:
+                break;
+            default:
+                break;        }
     }
     // while (1) {
     //     static struct option long_options[] = {
