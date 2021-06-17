af826768d55a675d58e0b7c89c7e7d02f42627c9
Matthew Dippel <mdip226@gmail.com>
2021-06-17 16:13:14 -0400

    remove prints

--- gillespy2/solvers/cpp/c_base/arg_parser.h
+++ gillespy2/solvers/cpp/c_base/arg_parser.h
@@ -39,15 +39,11 @@ usage: [simulation.out] \
             return 'I';
         if (!strcmp(opt, "--parameters"))
             return 'p';
-        if (!strcmp(opt, "--trajectories")){
-            // printf("match");
+        if (!strcmp(opt, "--trajectories"))
             return 'T';
-        }
-        
         if (!strcmp(opt, "--tau_tol"))
             return 0;
-        else
-        {
+        else{
             printf(usage);
         }
               
@@ -60,8 +56,6 @@ public:
     double increment = 0.0;
     double switch_tol = 0.0;
     double tau_tol = 0.0;
-    // std::vector<int> init_pop = std::vector<int>();
-    // std::vector<double> parameters = std::vector<double>();
 
     ArgParser(const int argc, char *argv[]);
     ~ArgParser();
@@ -70,44 +64,34 @@ public:
 ArgParser::ArgParser(int argc, char* argv[])    
 {
     int opt;
-    // printf("hello2\n");
-    // std::cerr << "SUP";
+
     for (int i = 1; i < argc; ++i){
-        // printf("%s strlen(argv[%d]): %d\n",argv[i],i,strlen(argv[i]));
         if (strlen(argv[i]) > 1 && argv[i][0] == '-' && argv[i][1] != '-') {
             opt = argv[i][1];
         }
         else if (strlen(argv[i]) > 1 && argv[i][0] == '-' && argv[i][1] == '-')
         {
-            // printf("long\n");
             opt = match_arg(argv[i]);
         }
         else {
             continue;
         }
-        // printf("%c\n", opt);
         std :: stringstream arg_stream(argv[i+1]);
         switch(opt) {
             case 't':
                 arg_stream >> timesteps;
-                // printf("timesteps: %d\n", timesteps);
                 break;
             case 'e':
                 arg_stream >> end;
-                // printf("end: %f\n", end);
-                // end = strtod(argv[i+1],0);
                 break;
             case 's':
                 arg_stream >> seed;
-                // seed = atoi(argv[i+1]);
                 break;
             case 'S':
                 arg_stream >> switch_tol;
-                // switch_tol = strtod(argv[i+1],0);
                 break;
             case 'i':
                 arg_stream >> increment;
-                // increment = strtod(argv[i + 1], 0);
                 break;
             case 'I':
                 Gillespy::map_variable_populations(arg_stream);
@@ -117,28 +101,17 @@ ArgParser::ArgParser(int argc, char* argv[])
                 break;
             case 'T':
                 arg_stream >> trajectories;
-                // printf("trajectories: %d\n", trajectories);
-                // trajectories = atoi(argv[i+1]);
                 break;
             case 0:
                 arg_stream >> tau_tol;
-                // tau_tol = strtod(argv[i + 1], 0);
                 break;
             default:
-                std::cerr << "error in option matching" << std::endl;
+                printf(usage);
                 break;
         }        
     }
 
 }
 
-ArgParser::~ArgParser()
-{
-    // delete args;
+ArgParser::~ArgParser(){
 }
-// int main(int argc, char *argv[])
-// {
-//     ArgParser parser = ArgParser(argc, argv);
-//     std::cout << parser.get("timesteps");
-//     return 0;
-// }
