f7be14ab58b4d9a8e4ba528511302b9c47eabe6e
ethangreen-dev <egreen4@unca.edu>
2021-06-21 14:16:57 -0400

    Updated version string to 1.6.0

--- gillespy2/__version__.py
+++ gillespy2/__version__.py
@@ -6,7 +6,7 @@
 # =============================================================================
 
 
-__version__      = '1.5.11'
+__version__      = '1.6.0'
 
 __title__        = 'GillesPy2'
 __description__  = 'Python interface for Gillespie-style biochemical simulations'
