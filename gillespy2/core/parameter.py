07ccb6fe5c34d14047d5d4d6286ef156fac9b731
JoshuaFisherC <70917271+JoshuaFisherC@users.noreply.github.com>
2021-10-21 14:00:47 -0400

    Update parameter.py

--- gillespy2/core/parameter.py
+++ gillespy2/core/parameter.py
@@ -42,7 +42,7 @@ class Parameter(SortableObject, Jsonify):
         # will be caught below. It is perfectly fine to give a scalar value as the expression.
         # This can then be evaluated in an empty namespace to the scalar value.
         if expression is None:
-            raise ParameterError("Parameter expression can not be none".format(param))
+            raise ParameterError("Parameter expression can not be none".format(params))
         
         self.expression = str(expression)
 
