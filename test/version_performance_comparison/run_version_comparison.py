af7802d88b27474f002737f29013f065668bbc81
Brian Drawert <brian@drawert.net>
2022-02-10 12:45:48 -0800

    code cleanup

--- test/version_performance_comparison/run_version_comparison.py
+++ test/version_performance_comparison/run_version_comparison.py
@@ -7,41 +7,35 @@ import re
 versions = [
 ]
 
-cmd3 = f"git clone https://github.com/StochSS/GillesPy2"
-print(cmd3)
-output3 = subprocess.run(cmd3, shell=True, capture_output=True)
-
-cmd4 = f"cd GillesPy2; git tag"
-print(cmd4)
-output4 = subprocess.run(cmd4, shell=True, capture_output=True)
-versions = output4.stdout.decode("utf-8").split("\n")
-
-
 def SortFn(ver):
     x = re.findall("\d+",ver)
     return int(x[0])*10000+int(x[1])*100+int(x[2])
 
-while("" in versions) :
-    versions.remove("")
+if __name__=="__main__":
+    cmd3 = f"git clone https://github.com/StochSS/GillesPy2"
+    print(cmd3)
+    output3 = subprocess.run(cmd3, shell=True, capture_output=True)
 
-versions.sort(key=SortFn)
+    cmd4 = f"cd GillesPy2; git tag"
+    print(cmd4)
+    output4 = subprocess.run(cmd4, shell=True, capture_output=True)
+    versions = output4.stdout.decode("utf-8").split("\n")
 
-#print(f"versions={versions}")
+    while("" in versions) :
+        versions.remove("")
 
+    versions.sort(key=SortFn)
 
-for ver in versions:
-    print(f"{ver}: ",end='')
-    cmd2 = f"cd GillesPy2; git checkout {ver}"
-    output2 = subprocess.run(cmd2, shell=True, capture_output=True)
-    cmd = f'export PYTHONPATH="{os.getcwd()}/GillesPy2"; python3 ./tyson_oscillator.py'
-    #print(cmd)
+    for ver in versions:
+        print(f"{ver}: ",end='')
+        cmd2 = f"cd GillesPy2; git checkout {ver}"
+        output2 = subprocess.run(cmd2, shell=True, capture_output=True)
+        cmd = f'export PYTHONPATH="{os.getcwd()}/GillesPy2"; python3 ./tyson_oscillator.py'
 
-    output = subprocess.run(cmd, shell=True, capture_output=True)
-    out = output.stdout.decode("utf-8").strip()
-    try:
-        print(float(out))
-    except:
-        print("error");
-        #print(output)
-
+        output = subprocess.run(cmd, shell=True, capture_output=True)
+        out = output.stdout.decode("utf-8").strip()
+        try:
+            print(float(out))
+        except:
+            print("error");
 
