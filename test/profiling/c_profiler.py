e10f4fd1250647ccaaac10903591092236494369
Joshua Cooper <jcooper6@unca.edu>
2022-03-19 17:09:00 -0400

    Profiler CLI updates
    Use `test/profiling/profiler.py` to run basic performance profiling.
    
    - Removed relative imports in profiler files
    - Added CLI args to script

--- test/profiling/c_profiler.py
+++ test/profiling/c_profiler.py
@@ -17,15 +17,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import os
+import pathlib
 import re
 import subprocess
 import tempfile
 import time
 from pathlib import Path
 
-from .performance_data import PerformanceData
-from .performance_data import PerformanceEntry
-
+import performance_data
 from gillespy2.core import Model
 from gillespy2.solvers.cpp.c_solver import CSolver
 from gillespy2.solvers.cpp.build.build_engine import BuildEngine
@@ -54,10 +53,10 @@ def parse_gprof_output(output: str):
         block = [b for b in block[3:] if not b.replace(".", "", 1).isnumeric()]
         call_name = "".join(block)
 
-        result = PerformanceEntry(t=t, percent=percent)
+        result = performance_data.PerformanceEntry(t=t, percent=percent)
         return call_name, result, float(cumulative_t)
 
-    results = PerformanceData()
+    results = performance_data.PerformanceData()
     total_time = 0
     worst_time = -1
 
@@ -80,7 +79,74 @@ def parse_gprof_output(output: str):
     return results
 
 
-def run_profiler(model: Model, solver: "str", trajectories=4, timesteps=101, end_time=100):
+class CProfiler(object):
+    """
+    Wrapper around compiled model+solver executable, used for retrieving profiler data.
+    """
+    def __init__(self, root_dir: "Path", exe_path: "str"):
+        self.root_dir = root_dir
+        self.exe = exe_path
+
+    @staticmethod
+    def build(root: "str", model: Model, solver: "str") -> "CProfiler":
+        build = BuildEngine(output_dir=root)
+        definitions = {
+            "CXXFLAGS": "-pg -std=c++14 -O0",
+            "CFLAGS": "-pg -O0",
+        }
+
+        # Prepare the simulation executable.
+        build.prepare(model)
+        exe = build.build_simulation(simulation_name=solver, definitions=definitions)
+
+        return CProfiler(root_dir=pathlib.Path(root), exe_path=exe)
+
+    def run(self, trajectories=4, timesteps=101, end_time=100) -> "performance_data.PerformanceData":
+        # Execute the simulation before running the profiler.
+        # When profiling compiler flags (-pg) are enabled,
+        #  running the program outputs a profiling data file:
+        #  f"{output_dir}/profile.{pid}"
+        # This profiler data can then be parsed using gprof.
+        process_args = [
+            self.exe,
+            "-T", str(trajectories),
+            "-t", str(timesteps),
+            "-e", str(end_time),
+            "-i", str(end_time / (timesteps - 1))
+        ]
+        start = time.perf_counter()
+        subprocess.check_call(args=process_args, stdout=subprocess.DEVNULL, cwd=self.root_dir)
+        stop = time.perf_counter()
+
+        # Locate gprof profiling metadata in output directory.
+        # Gprof will always append `.{pid}` to the end of {GMON_OUT_PREFIX}
+        # As such, data is located by finding the file in the output dir
+        #   which contains the matching prefix defined above.
+        gprof_data: Path = None
+
+        for n in self.root_dir.iterdir():
+            if n.stem == "gmon":
+                gprof_data = self.root_dir.joinpath(n.name)
+                break
+
+        if gprof_data is None:
+            raise EnvironmentError(
+                "Profiler data was not found in the current environment; aborting"
+            )
+
+        # Run gprof to process the profiler output.
+        # -b = brief, shows minimal output
+        # -p = flat profile, discards call graph data
+        gprof_args = ["gprof", "-bp", self.exe, str(gprof_data)]
+        perf_out = subprocess.check_output(gprof_args, stderr=subprocess.DEVNULL).decode("utf-8")
+        gprof_data.unlink()
+
+        # Parse the resulting output
+        performance_results = parse_gprof_output(perf_out)
+        performance_results.execution_time = (stop - start) * 1000
+        return performance_results
+
+def run_profiler(model: Model, solver: "str", iterations: "int", trajectories=4, timesteps=101, end_time=100):
     """
     Run low-level performance tests on the specified solver.
     Profiling is performed with a system-level profiling tool.
@@ -88,62 +154,7 @@ def run_profiler(model: Model, solver: "str", trajectories=4, timesteps=101, end
 
     Output is automatically parsed and formatted.
     """
-    try:
-        with tempfile.TemporaryDirectory() as tmp:
-            build = BuildEngine(output_dir=tmp)
-            definitions = {
-                "CXXFLAGS": "-pg -std=c++14 -O0",
-                "CFLAGS": "-pg -O0",
-            }
-
-            # Prepare the simulation executable.
-            build.prepare(model)
-            exe = build.build_simulation(simulation_name=solver, definitions=definitions)
-
-            # Execute the simulation before running the profiler.
-            # When profiling compiler flags (-pg) are enabled,
-            #  running the program outputs a profiling data file:
-            #  f"{output_dir}/profile.{pid}"
-            # This profiler data can then be parsed using gprof.
-            process_args = [
-                exe,
-                "-T", str(trajectories),
-                "-t", str(timesteps),
-                "-e", str(end_time),
-                "-i", str(end_time / (timesteps - 1))
-            ]
-            start = time.perf_counter()
-            subprocess.check_call(args=process_args, stdout=subprocess.DEVNULL, cwd=tmp)
-            stop = time.perf_counter()
-
-            # Locate gprof profiling metadata in output directory.
-            # Gprof will always append `.{pid}` to the end of {GMON_OUT_PREFIX}
-            # As such, data is located by finding the file in the output dir
-            #   which contains the matching prefix defined above.
-            gprof_data: Path = None
-
-            for n in build.output_dir.iterdir():
-                if n.stem == "gmon":
-                    gprof_data = build.output_dir.joinpath(n.name)
-                    break
-
-            if gprof_data is None:
-                raise EnvironmentError(
-                    "Profiler data was not found in the current environment; aborting"
-                )
-
-            # Run gprof to process the profiler output.
-            # -b = brief, shows minimal output
-            # -p = flat profile, discards call graph data
-            gprof_args = ["gprof", "-bp", exe, str(gprof_data)]
-            perf_out = subprocess.check_output(gprof_args, stderr=subprocess.DEVNULL).decode("utf-8")
-            gprof_data.unlink()
-
-        # Parse the resulting output
-        performance_results = parse_gprof_output(perf_out)
-        performance_results.execution_time = (stop - start) * 1000
-
-        return performance_results
-
-    finally:
-        build.clean()
+    with tempfile.TemporaryDirectory() as tmp:
+        profiler = CProfiler.build(root=tmp, model=model, solver=solver)
+        for _ in range(iterations):
+            yield profiler.run(trajectories=trajectories, timesteps=timesteps, end_time=end_time)
--- test/profiling/performance_data.py
+++ test/profiling/performance_data.py
@@ -16,6 +16,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+from typing import Union
+
+
 class PerformanceEntry:
     perf_time: float = 0.0
     percent: float = 0.0
@@ -24,21 +27,55 @@ class PerformanceEntry:
         self.perf_time = float(t) * 1000
         self.percent = float(percent)
 
-class PerformanceData:
+
+class ExecutionData(object):
     # Value in ms representing time spent overall by the executable.
     # Measures as the time from start to the time it returns.
-    execution_time: float = -1.0
+    execution_time: float = 0.0
 
     # Value in ms representing time spent on the program's user-defined call stack.
     # Measured as the cumulative time spent on each sampled function of the program.
-    sample_time: float = -1.0
+    sample_time: float = 0.0
 
+    def __init__(self, execution_time: "float" = 0.0, sample_time: "float" = 0.0):
+        self.execution_time = execution_time
+        self.sample_time = sample_time
+
+    def __add__(self, other: "Union[int, float, ExecutionData]"):
+        if isinstance(other, ExecutionData):
+            return ExecutionData(
+                execution_time=self.execution_time + other.execution_time,
+                sample_time=self.sample_time + other.sample_time,
+            )
+        
+        return ExecutionData(
+            execution_time=self.execution_time + other,
+            sample_time=self.sample_time + other,
+        )
+
+    def __truediv__(self, other: "Union[int, float, ExecutionData]"):
+        if isinstance(other, ExecutionData):
+            return ExecutionData(
+                execution_time=self.execution_time / other.execution_time,
+                sample_time=self.sample_time / other.sample_time,
+            )
+        
+        return ExecutionData(
+            execution_time=self.execution_time / other,
+            sample_time=self.sample_time / other,
+        )
+
+class PerformanceData(ExecutionData):
     call_list: "dict[str, PerformanceEntry]" = None
     worst_entry: "tuple[str, PerformanceEntry]" = None
 
-    def __init__(self):
-        self.call_list = {}
-        self.worst_entry = ("unknown", PerformanceEntry())
+    def __init__(self, call_list=None, worst_entry=None):
+        if call_list is None:
+            call_list = {}
+        super().__init__()
+
+        self.call_list = call_list
+        self.worst_entry = worst_entry
 
     def __str__(self):
         string = f"Total execution time: {round(self.execution_time, 6)}ms ({self.sample_time}ms sampled)\n"
@@ -46,3 +83,34 @@ class PerformanceData:
             string += f"[{call_key}]: {call_entry.perf_time}ms ({call_entry.percent}%)\n"
 
         return string
+
+class PerformanceEnsemble:
+    """
+    Container class for storing and operating on a sequence of profiler runs.
+    Each run is made available, and statical methods can be applied on the ensemble results.
+    """
+
+    runs: "list[PerformanceData]" = None
+    def __init__(self):
+        self.runs = []
+
+    def get_average_time(self) -> "ExecutionData":
+        """
+        Compute the average overall performance time of all profiler runs in the collection.
+
+        :rtype: ExecutionData
+        :returns: Data structure indicating the average sample time and execution time.
+        """
+        return self.get_total_time() / len(self.runs)
+
+    def get_total_time(self) -> "ExecutionData":
+        """
+        Compute the total overall performance time of all profiler runs in the collection.
+
+        :rtype: ExecutionData
+        :returns: Data structure indicating the total sample time and execution time.
+        """
+        return ExecutionData(
+            execution_time=sum([run.execution_time for run in self.runs]),
+            sample_time=sum([run.sample_time for run in self.runs]),
+        )
