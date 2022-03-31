fbbb7533d57f53c3992698e25bfbbb29152bc593
Bryan Rumsey <bmrumsey@gmail.com>
2022-03-31 19:17:55 -0400

    Added unit tests for Model.add.

--- test/unit_tests/test_model.py
+++ test/unit_tests/test_model.py
@@ -27,6 +27,112 @@ class TestModel(unittest.TestCase):
     def setUp(self):
         self.model = RobustModel()
 
+    def test_model_add__assignment_rule(self):
+        from gillespy2 import AssignmentRule
+        ar1 = AssignmentRule(name="ar1", variable="k1", formula="29")
+        self.model.add(ar1)
+        self.assertIn("ar1", self.model.listOfAssignmentRules)
+
+    def test_model_add__event(self):
+        from gillespy2 import Event, EventTrigger, EventAssignment
+        ea = EventAssignment(name="ea", variable="k1", expression="29")
+        et = EventTrigger(expression="t > 29")
+        e2 = Event(name="e2", trigger=et, assignments=[ea])
+        self.model.add(e2)
+        self.assertIn("e2", self.model.listOfEvents)
+
+    def test_model_add__function_definition(self):
+        from gillespy2 import FunctionDefinition
+        divide = FunctionDefinition(name="divide", function="x / y", args=["x", "y"])
+        self.model.add(divide)
+        self.assertIn("divide", self.model.listOfFunctionDefinitions)
+
+    def test_model_add__invalid_component(self):
+        with self.assertRaises(ModelError):
+            self.model.add("29")
+
+    def test_model_add__parameter(self):
+        from gillespy2 import Parameter
+        k3 = Parameter(name="k3", expression=29)
+        self.model.add(k3)
+        self.assertIn("k3", self.model.listOfParameters)
+
+    def test_model_add__rate_rule(self):
+        from gillespy2 import RateRule
+        rr3 = RateRule(name="rr3", variable="k1", formula="29")
+        self.model.add(rr3)
+        self.assertIn("rr3", self.model.listOfRateRules)
+
+    def test_model_add__reaction(self):
+        from gillespy2 import Reaction
+        r4 = Reaction(name="r4", reactants={"s1": 1}, products={"s2": 1}, rate="k1")
+        self.model.add(r4)
+        self.assertIn("r4", self.model.listOfReactions)
+
+    def test_model_add__species(self):
+        from gillespy2 import Species
+        s3 = Species(name="s3", initial_value=29)
+        self.model.add(s3)
+        self.assertIn("s3", self.model.listOfSpecies)
+
+    def test_model_add__timespan(self):
+        from gillespy2 import TimeSpan
+        tspan = TimeSpan(range(100))
+        self.model.add(tspan)
+        self.assertEqual(tspan, self.model.tspan)
+
+    def test_model_add__multiple_components__in_order(self):
+        import gillespy2
+
+        s1 = gillespy2.Species(name="s1", initial_value=29)
+        k1 = gillespy2.Parameter(name="k1", expression=29)
+        r1 = gillespy2.Reaction(name="r1", reactants={"s1": 1}, rate="k1")
+        rr1 = gillespy2.RateRule(name="rr1", variable="k1", formula="29")
+        ar1 = gillespy2.AssignmentRule(name="ar1", variable="k1", formula="29")
+        ea = gillespy2.EventAssignment(name="ea", variable="k1", expression="29")
+        et = gillespy2.EventTrigger(expression="t > 29")
+        e1 = gillespy2.Event(name="e1", trigger=et, assignments=[ea])
+        divide = gillespy2.FunctionDefinition(name="divide", function="x / y", args=["x", "y"])
+        tspan = TimeSpan(range(100))
+
+        model = gillespy2.Model(name="Test Model")
+        model.add([s1, k1, r1, rr1, ar1, e1, divide, tspan])
+
+        self.assertIn("ar1", model.listOfAssignmentRules)
+        self.assertIn("e1", model.listOfEvents)
+        self.assertIn("divide", model.listOfFunctionDefinitions)
+        self.assertIn("k1", model.listOfParameters)
+        self.assertIn("rr1", model.listOfRateRules)
+        self.assertIn("r1", model.listOfReactions)
+        self.assertIn("s1", model.listOfSpecies)
+        self.assertEqual(tspan, model.tspan)
+
+    def test_model_add__multiple_components__in_order(self):
+        import gillespy2
+
+        s1 = gillespy2.Species(name="s1", initial_value=29)
+        k1 = gillespy2.Parameter(name="k1", expression=29)
+        r1 = gillespy2.Reaction(name="r1", reactants={"s1": 1}, rate="k1")
+        rr1 = gillespy2.RateRule(name="rr1", variable="k1", formula="29")
+        ar1 = gillespy2.AssignmentRule(name="ar1", variable="k1", formula="29")
+        ea = gillespy2.EventAssignment(name="ea", variable="k1", expression="29")
+        et = gillespy2.EventTrigger(expression="t > 29")
+        e1 = gillespy2.Event(name="e1", trigger=et, assignments=[ea])
+        divide = gillespy2.FunctionDefinition(name="divide", function="x / y", args=["x", "y"])
+        tspan = TimeSpan(range(100))
+
+        model = gillespy2.Model(name="Test Model")
+        model.add([ar1, divide, e1, k1, s1, r1, rr1, tspan])
+
+        self.assertIn("ar1", model.listOfAssignmentRules)
+        self.assertIn("e1", model.listOfEvents)
+        self.assertIn("divide", model.listOfFunctionDefinitions)
+        self.assertIn("k1", model.listOfParameters)
+        self.assertIn("rr1", model.listOfRateRules)
+        self.assertIn("r1", model.listOfReactions)
+        self.assertIn("s1", model.listOfSpecies)
+        self.assertEqual(tspan, model.tspan)
+
     def test_delete_assignment_rule(self):
         self.model.delete_assignment_rule('rr2')
         self.assertNotIn('rr2', self.model.listOfAssignmentRules)
