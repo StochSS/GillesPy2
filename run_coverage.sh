6fee9c999157b41bce8ffb76fa1e5cb22f5c623e
Bryan Rumsey <bmrumsey@gmail.com>
2022-05-11 10:23:34 -0400

    Renamed the run_tests script to run_integration_tests.

--- run_coverage.sh
+++ run_coverage.sh
@@ -2,6 +2,6 @@
 #pip3 install  coverage
 #pip3 install python-libsbml
 
-coverage run --source=gillespy2 --omit=*gillespy2/solvers/stochkit/*,*gillespy2/solvers/cython/* test/run_tests.py -m develop
+coverage run --source=gillespy2 --omit=*gillespy2/solvers/stochkit/*,*gillespy2/solvers/cython/* test/run_integration_tests.py -m develop
 coverage run --source=gillespy2 --omit=*gillespy2/solvers/stochkit/*,*gillespy2/solvers/cython/* test/run_unit_tests.py -m develop
 coverage html
