3ac3c6d26dbc698194c47e268312fd7475bfc698
Bryan Rumsey <bmrumsey@gmail.com>
2022-05-19 13:47:55 -0400

    Examples update part 3.

--- examples/Models/ExtraModels/tyson_oscillator.py
+++ examples/Models/ExtraModels/tyson_oscillator.py
@@ -19,12 +19,21 @@
 # 2019-06-13 Sean Matthew
 # =============================================================================
 
-import numpy as np
+
+# Tyson 2 State Oscillator
+# .............................................................................
+# Here, as a test case, we run a simple two-state oscillator
+# (Novak & Tyson 2008) as an example of a stochastic reaction system.
+
+# Setup the Evironment
+# .............................................................................
+import os
+import sys
+sys.path.insert(1, os.path.abspath(os.path.join(os.getcwd(), '../../..')))
+
+# MatPlotLib is used for creating custom visualizations
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
-import sys
-
-sys.path.append('../..')
 
 try:
     import gillespy2
@@ -41,100 +50,82 @@ except:
     sys.exit()
 
 
-# Model definition.
+# Build the Tyson Oscillator Model
 # .............................................................................
-# In GillesPy2, a model to be simulated is expressed as an object having the
-# parent class "Model".  Components of the model to be simulated, such as the
-# reactions, molecular species, and the time span for simualtion, are all
-# defined within the class definition.
+# Parameter values  for this biochemical system are given in concentration
+# units. However, stochastic systems must use population values. For example,
+# a concentration unit of 0.5mol/(L*s) is multiplied by a volume unit, to get a
+# population/s rate constant. Thus, for our non-mass action reactions, we
+# include the parameter "vol" in order to convert population units to
+# concentration units. Volume here = 300.
 
-class Tyson2StateOscillator(gillespy2.Model):
-    """
-    Here, as a test case, we run a simple two-state oscillator (Novak & Tyson
-    2008) as an example of a stochastic reaction system.
-    """
-    def __init__(self, parameter_values=None):
-        """
-        """
-        system_volume = 300 #system volume
-        gillespy2.Model.__init__(self, name="tyson-2-state", volume=system_volume)
-        self.timespan(np.linspace(0,100,101))
+def build_tyson_oscillator(parameter_values=None):
+    model = gillespy2.Model(name="tyson-2-state")
+    
+    # Set System Volume
+    model.volume = 300
+    
+    # Define Species
+    # Initial values of each species (concentration converted to pop.)
+    X = gillespy2.Species(name='X', initial_value=int(0.65609071 * model.volume))
+    Y = gillespy2.Species(name='Y', initial_value=int(0.85088331 * model.volume))
+    
+    # Add Species to Model
+    model.add_species([X, Y])
 
-        # =============================================
-        # Define model species, initial values, parameters, and volume
-        # =============================================
+    # Define Parameters
+    P   = gillespy2.Parameter(name='P', expression=2.0)
+    kt  = gillespy2.Parameter(name='kt', expression=20.0)
+    kd  = gillespy2.Parameter(name='kd', expression=1.0)
+    a0  = gillespy2.Parameter(name='a0', expression=0.005)
+    a1  = gillespy2.Parameter(name='a1', expression=0.05)
+    a2  = gillespy2.Parameter(name='a2', expression=0.1)
+    kdx = gillespy2.Parameter(name='kdx', expression=1.0)
+    
+    # Add Parameters to Model
+    model.add_parameter([P, kt, kd, a0, a1, a2, kdx])
 
-        # Parameter values  for this biochemical system are given in
-        # concentration units. However, stochastic systems must use population
-        # values. For example, a concentration unit of 0.5mol/(L*s)
-        # is multiplied by a volume unit, to get a population/s rate
-        # constant. Thus, for our non-mass action reactions, we include the
-        # parameter "vol" in order to convert population units to concentration
-        # units. Volume here = 300.
+    # Define Reactions
+    # creation of X:
+    rxn1 = gillespy2.Reaction(
+        name='X production', reactants={}, products={X: 1}, propensity_function='vol*1/(1+(Y*Y/((vol*vol))))'
+    )
+    # degradadation of X:
+    rxn2 = gillespy2.Reaction(name='X degradation', reactants={X: 1}, products={}, rate=kdx)
+    # creation of Y:
+    rxn3 = gillespy2.Reaction(name='Y production', reactants={X: 1}, products={X: 1, Y: 1}, rate=kt)
+    # degradation of Y:
+    rxn4 = gillespy2.Reaction(name='Y degradation', reactants={Y: 1}, products={}, rate=kd)
+    # nonlinear Y term:
+    rxn5 = gillespy2.Reaction(
+        name='Y nonlin', reactants={Y: 1}, products={}, propensity_function='Y/(a0 + a1*(Y/vol)+a2*Y*Y/(vol*vol))'
+    )
 
-        P   = gillespy2.Parameter(name='P', expression=2.0)
-        kt  = gillespy2.Parameter(name='kt', expression=20.0)
-        kd  = gillespy2.Parameter(name='kd', expression=1.0)
-        a0  = gillespy2.Parameter(name='a0', expression=0.005)
-        a1  = gillespy2.Parameter(name='a1', expression=0.05)
-        a2  = gillespy2.Parameter(name='a2', expression=0.1)
-        kdx = gillespy2.Parameter(name='kdx', expression=1.0)
-        self.add_parameter([P, kt, kd, a0, a1, a2, kdx])
+    # Add Reactions to Model
+    model.add_reaction([rxn1, rxn2, rxn3, rxn4, rxn5])
 
-        # Species
-        # Initial values of each species (concentration converted to pop.)
-        X = gillespy2.Species(name='X', initial_value=int(0.65609071*system_volume))
-        Y = gillespy2.Species(name='Y', initial_value=int(0.85088331*system_volume))
-        self.add_species([X, Y])
+    # Define Timespan
+    tspan = gillespy2.TimeSpan.linspace(t=20, num_points=21)
+    
+    # Set Model Timespan
+    model.timespan(tspan)
+    return model
 
-        # =============================================
-        # Define the reactions within the model
-        # =============================================
-
-        # creation of X:
-        rxn1 = gillespy2.Reaction(name = 'X production',
-                                  reactants = {},
-                                  products = {X:1},
-                                  propensity_function = 'vol*1/(1+(Y*Y/((vol*vol))))')
-
-        # degradadation of X:
-        rxn2 = gillespy2.Reaction(name = 'X degradation',
-                                  reactants = {X:1},
-                                  products = {},
-                                  rate = kdx)
-
-        # creation of Y:
-        rxn3 = gillespy2.Reaction(name = 'Y production',
-                                  reactants = {X:1},
-                                  products = {X:1, Y:1},
-                                  rate = kt)
-
-        # degradation of Y:
-        rxn4 = gillespy2.Reaction(name = 'Y degradation',
-                                  reactants = {Y:1},
-                                  products = {},
-                                  rate = kd)
-
-        # nonlinear Y term:
-        rxn5 = gillespy2.Reaction(name = 'Y nonlin',
-                                  reactants = {Y:1},
-                                  products = {},
-                                  propensity_function = 'Y/(a0 + a1*(Y/vol)+a2*Y*Y/(vol*vol))')
-
-        self.add_reaction([rxn1,rxn2,rxn3,rxn4,rxn5])
-
-
-# Model simulation.
-# .............................................................................
 
 if __name__ == '__main__':
-    # A simulation in GillesPy2 is performed by first instantiating the model
-    # to be simulated, and then invoking the "run" method on that object.
-    # The results of the simulation are the output of "run".
+    # Initialize the Model
+    tyson_model = build_tyson_oscillator()
 
-    tyson_model = Tyson2StateOscillator()
-    trajectories = tyson_model.run(show_labels=False)
+    
+    # Run the Simulations
+    # .............................................................................
+    results = tyson_model.run()
+    
+    
+    # Processing Results
+    trajectories = results.to_array()
 
+    # Visualization
     # =============================================
     # plot just the first trajectory, 0, in both time and phase space:
     # =============================================
