2519074eabf0136fe7a64c3474f51ae402b7e644
Bryan Rumsey <bmrumsey@gmail.com>
2022-06-05 09:55:31 -0400

    Uncommented the integration tests and removed the test with spaces.

--- test/run_integration_tests.py
+++ test/run_integration_tests.py
@@ -33,60 +33,58 @@ if __name__ == '__main__':
         print('Running tests in develop mode. Appending repository directory to system path.')
         sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-#    import test_empty_model
-#    import test_build_engine
-#    import test_c_solvers
-#    import test_model
-#    import test_ode_solver
-#    import test_ode_c_solver
-#    import test_hybrid_solver
-#    import test_simple_model
-#    import test_ssa_c_solver
-#    import test_variable_solvers
-#    import test_tau_leaping_c_solver
-#    import test_tau_leaping_solver
-#    import test_tau_hybrid_c_solver
-#    import test_SBML
-#    import test_StochML
-#    import test_example_models
-#    import test_all_solvers
-#    import test_sys_init
-#    import test_results
-#    import test_propensity_parser
-#    import test_pause_resume
-#    import test_check_cpp_support
-#    import test_jsonify
-#    import test_notebooks
-#    import test_c_decode
-    import test_compile_w_spaces
+   import test_empty_model
+   import test_build_engine
+   import test_c_solvers
+   import test_model
+   import test_ode_solver
+   import test_ode_c_solver
+   import test_hybrid_solver
+   import test_simple_model
+   import test_ssa_c_solver
+   import test_variable_solvers
+   import test_tau_leaping_c_solver
+   import test_tau_leaping_solver
+   import test_tau_hybrid_c_solver
+   import test_SBML
+   import test_StochML
+   import test_example_models
+   import test_all_solvers
+   import test_sys_init
+   import test_results
+   import test_propensity_parser
+   import test_pause_resume
+   import test_check_cpp_support
+   import test_jsonify
+   import test_notebooks
+   import test_c_decode
 
     modules = [
-#        test_empty_model,
-#        test_build_engine,
-#        test_c_solvers,
-#        test_model,
-#        test_ode_solver,
-#        test_ode_c_solver,
-#        test_tau_leaping_c_solver,
-#        test_tau_leaping_solver,
-#        test_tau_hybrid_c_solver,
-#        test_hybrid_solver,
-#        test_simple_model,
-#        test_ssa_c_solver,
-#        test_variable_solvers,
-#        test_pause_resume,
-#        test_SBML,
-#        test_StochML,
-#        test_example_models,
-#        test_all_solvers,
-#        test_sys_init,
-#        test_results,
-#        test_propensity_parser,
-#        test_check_cpp_support,
-#        test_jsonify,
-#        test_notebooks,
-#        test_c_decode,
-        test_compile_w_spaces
+       test_empty_model,
+       test_build_engine,
+       test_c_solvers,
+       test_model,
+       test_ode_solver,
+       test_ode_c_solver,
+       test_tau_leaping_c_solver,
+       test_tau_leaping_solver,
+       test_tau_hybrid_c_solver,
+       test_hybrid_solver,
+       test_simple_model,
+       test_ssa_c_solver,
+       test_variable_solvers,
+       test_pause_resume,
+       test_SBML,
+       test_StochML,
+       test_example_models,
+       test_all_solvers,
+       test_sys_init,
+       test_results,
+       test_propensity_parser,
+       test_check_cpp_support,
+       test_jsonify,
+       test_notebooks,
+       test_c_decode,
     ]
 
     for module in modules:
