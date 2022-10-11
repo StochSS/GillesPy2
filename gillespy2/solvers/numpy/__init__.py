e45672949d7c4ba7993c8bc83d01a5cc87700512
Bryan Rumsey <bmrumsey@gmail.com>
2022-10-11 10:09:06 -0400

    Removed the try catch block from the numpy module init.

--- gillespy2/solvers/numpy/__init__.py
+++ gillespy2/solvers/numpy/__init__.py
@@ -22,19 +22,12 @@ log = logging.getLogger()
 log.setLevel(logging.WARN)
 log.addHandler(_handler)
 
-try:
-    import numpy as np
-    can_use_numpy = True
-    from .ssa_solver import NumPySSASolver
-    from .ode_solver import ODESolver
-    from .tau_leaping_solver import TauLeapingSolver
-    from .tau_hybrid_solver import TauHybridSolver
-    from .CLE_solver import CLESolver
-    log.debug("Successful Import of NumPy solvers.")
-
-except Exception as e:
-    log.warn(" Unable to use NumPy: {0}. The performance of this package can be significantly increased if you install NumPy.".format(e))
-    can_use_numpy = False
-
+import numpy as np
+from .ssa_solver import NumPySSASolver
+from .ode_solver import ODESolver
+from .tau_leaping_solver import TauLeapingSolver
+from .tau_hybrid_solver import TauHybridSolver
+from .CLE_solver import CLESolver
+log.debug("Successful Import of NumPy solvers.")
 
 __all__ = ['NumPySSASolver', 'ODESolver', 'TauLeapingSolver', 'TauHybridSolver', 'CLESolver'] if can_use_numpy else []
