e6f52f2ec68786940dcfd00f76a3bf79bb83fa71
Brian Drawert <brian@drawert.net>
2022-12-09 16:27:35 -0800

    activating tests

--- test/run_integration_tests.py
+++ test/run_integration_tests.py
@@ -58,6 +58,7 @@ if __name__ == '__main__':
     import test_jsonify
     import test_notebooks
     import test_c_decode
+    import test_hybrid_negative_state
 
     modules = [
        test_empty_model,
@@ -85,6 +86,7 @@ if __name__ == '__main__':
        test_jsonify,
        test_notebooks,
        test_c_decode,
+       test_hybrid_negative_state,
     ]
 
     for module in modules:
