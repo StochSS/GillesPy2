e8b697e8a288f88534621849b22d59c4bbcbe74d
Matthew Dippel <mdip226@gmail.com>
2023-07-19 10:51:35 -0400

    handy dev feature

---
+++ gillespy2/remote/core/debug.py
@@ -0,0 +1,23 @@
+'''
+gillespy2.remote.core.debug
+'''
+# StochSS-Compute is a tool for running and caching GillesPy2 simulations remotely.
+# Copyright (C) 2019-2023 GillesPy2 and StochSS developers.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from logging import getLogger
+getLogger().setLevel('DEBUG')
+# Call the line below in a python process to initiate client-side logging
+# import gillespy2.remote.core.debug 
